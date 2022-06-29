7664ad14f44548cf9bf1aa1fd5ef5bb55755b785
oyuchan <oyc0401@gmail.com>
2022-06-29 16:59:11 +0900

    1152

--- main.cpp
+++ main.cpp
@@ -9,38 +9,29 @@ using namespace std;
 int main() {
     string str;
 
-    cin >> str;
+    getline(cin,str);
     map<char, int> map;
 
-    for (char &c: str) {
-        c = toupper(c);
+    int sum=0;
+    if(str[0]==' '){
+        sum--;
+    }
+    if(str[str.length()-1]==' '){
+        sum--;
     }
 
-    for (char &c: str) {
-        map[c]++;
-    }
 
-    int big = 0;
-    char bigText;
-    for (pair<char, int> pa: map) {
-        if (big < pa.second) {
-            big = pa.second;
-            bigText=pa.first;
+    for (char c: str) {
+        if(c==' '){
+            sum++;
         }
     }
+    cout<<sum+1;
 
-    int check=0;
-    for (pair<char, int> pa: map) {
-        if (big == pa.second) {
-           check++;
-        }
-        if(check==2){
-            cout<<"?";
-            return 0;
-        }
-    }
 
-    cout<<bigText;
+
+
+
 
     return 0;
 }
