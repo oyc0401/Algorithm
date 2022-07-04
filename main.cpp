520fe036370cc68f70873345ca853505203f5824
oyuchan <oyc0401@gmail.com>
2022-07-04 21:02:06 +0900

    1929 소수 구하기

--- main.cpp
+++ main.cpp
@@ -1,28 +1,44 @@
 
 #include <iostream>
+#include <vector>
 
 
 using namespace std;
 
+
+void setPrimeNumbers(int n, vector<int> &primes) {
+    bool *isPrime = new bool[n + 1];// n까지 구해야하므로, n+1개를 동적할당
+
+    //먼저 모든 배열을 true로 초기화
+    for (int i = 0; i <= n; i++) {
+        isPrime[i] = true;
+    }
+
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            primes.push_back(i);//해당수가 소수라면, 출력하고 해당수를 제외한 배수들을 모두 제외
+            // cout << i << " ";
+            for (int j = i * 2; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+}
+
 int main() {
-    int x,y,maxX,maxY;
-    cin>>x>>y>>maxX>>maxY;
 
-    int small=x;
+    int a,b;
+    cin>>a>>b;
 
-    if(small>y){
-        small=y;
+    vector<int> primes;
+    setPrimeNumbers(b, primes);
+
+    for (int pri:primes){
+        if(pri>=a){
+            cout<<pri<<"\n";
+        }
+
     }
-    if(small>maxX-x){
-        small=maxX-x;
-    }
-    if(small>maxY-y){
-        small=maxY-y;
-    }
-
-    cout<<small;
-
-
 
     return 0;
 }
