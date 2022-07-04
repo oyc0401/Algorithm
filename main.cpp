ac9c80554e35f1111145ec7a962180b56d691a0c
oyuchan <oyc0401@gmail.com>
2022-07-04 22:51:07 +0900

    1654 평균탐색

--- main.cpp
+++ main.cpp
@@ -5,40 +5,96 @@
 
 using namespace std;
 
+vector<int> ropes;
 
-void setPrimeNumbers(int n, vector<int> &primes) {
-    bool *isPrime = new bool[n + 1];// n까지 구해야하므로, n+1개를 동적할당
-
-    //먼저 모든 배열을 true로 초기화
-    for (int i = 0; i <= n; i++) {
-        isPrime[i] = true;
+// 몇개로 나뉘는지 함수
+int hownuch (int size) {
+    int sum = 0;
+    for (int rope: ropes) {
+        sum += rope / size;
     }
-
-    for (int i = 2; i <= n; i++) {
-        if (isPrime[i]) {
-            primes.push_back(i);//해당수가 소수라면, 출력하고 해당수를 제외한 배수들을 모두 제외
-            // cout << i << " ";
-            for (int j = i * 2; j <= n; j += i) {
-                isPrime[j] = false;
-            }
-        }
-    }
+    // cout << size << "cm" << ": " << sum << "개" << endl;
+    return sum;
 }
+
+//4 300
+//802
+//743
+//457
+//539
+
+// 4 13
+// 802
+// 743
+// 457
+// 539
+
+int hownuchNaMuZi(int size) {
+    int max = 0;
+    int min = 10000000;
+
+    for (int rope: ropes) {
+        int namuzi = rope % size;
+        if (min > namuzi) {
+            min = namuzi;
+            max = rope / size;
+        }
+       // cout << rope << ": " << size << " * " << rope / size << " + " << namuzi << endl;
+    }
+
+
+    return min / max;
+
+}
+
 
 int main() {
 
-    int a,b;
-    cin>>a>>b;
 
-    vector<int> primes;
-    setPrimeNumbers(b, primes);
+    int K, N;
+    cin >> K >> N;
 
-    for (int pri:primes){
-        if(pri>=a){
-            cout<<pri<<"\n";
+
+    int big = 0;
+    int min = 1;
+
+    // 배열에 넣기
+    for (int i = 0; i < K; i++) {
+        int a;
+        cin >> a;
+        ropes.push_back(a);
+        if (big < a) {
+            big = a;
+        }
+    }
+
+    // 구하기
+    long long int middle = (big + min) / 2;
+
+    int num = hownuch( middle);
+
+    while (num != N) {
+
+        if (num < N) {
+            big = middle;
+        } else if (num > N) {
+            min = middle;
+        } else {
         }
 
+        if (middle == (big + min) / 2) {
+            break;
+        }
+        middle = (big + min) / 2;
+        //cout << min << ", " << middle << ", " << big << endl;
+        num = hownuch( middle);
+        //cout << current << ", " << num << endl;
     }
+
+    int plus = hownuchNaMuZi(middle);
+
+    cout << middle + plus;
+
 
     return 0;
 }
