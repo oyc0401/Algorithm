c1412a2eb2a769bcf5c4e382104c98992c2203eb
oyuchan <oyc0401@gmail.com>
2022-07-09 23:32:27 +0900

    10845 큐

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 
 #include <iostream>
-#include <vector>
+
 #include <algorithm>
 #include <set>
-#include <stack>
+#include <queue>
 
 
 using namespace std;
@@ -11,39 +11,42 @@ using namespace std;
 
 int main() {
 
-
+    queue<int> que;
     int a;
     cin >> a;
-
-
     for (int i = 0; i < a; ++i) {
-        int left = 0;
-        bool yes = true;
+        string message;
 
-        string galho;
-        cin >> galho;
-        //cout << galho.size();
-        for (int k = 0; k < galho.size(); ++k) {
-            //cout<<galho[k]<<endl;
-            if (galho[k] == '(') {
-                left++;
-            } else if (galho[k] == ')') {
-                if (left > 0) {
-                    left--;
-                } else {
-                    yes = false;
-                    break;
-                }
+        cin >> message;
+
+        if (message == "push") {
+            int number;
+            cin >> number;
+            que.push(number);
+        } else if (message == "pop") {
+            if(que.empty()){
+                cout<<"-1"<<"\n";
+            }else{
+                cout << que.front() << "\n";
+                que.pop();
             }
 
-        }
-        if(left>0){
-            yes= false;
-        }
-        if (yes) {
-            cout << "YES"<<endl;
-        } else {
-            cout << "NO" << endl;
+        } else if (message == "size") {
+            cout << que.size() << "\n";
+        } else if (message == "empty") {
+            cout << que.empty() << "\n";
+        } else if (message == "front") {
+            if(que.empty()){
+                cout<<"-1"<<"\n";
+            }else{
+                cout << que.front() << "\n";
+            }
+        }else if (message == "back") {
+            if(que.empty()){
+                cout<<"-1"<<"\n";
+            }else{
+                cout << que.back() << "\n";
+            }
         }
 
     }
