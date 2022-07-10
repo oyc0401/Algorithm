a42850a07e7e26961701a4dc9856a74abbd2b059
oyuchan <oyc0401@gmail.com>
2022-07-10 21:10:57 +0900

    1018 체스판 다시 칠하기

--- main.cpp
+++ main.cpp
@@ -8,48 +8,112 @@
 
 using namespace std;
 
+bool ff[50][50];
+
+int getHow(int a, int b) {
+    int white = 0;
+    int black = 0;
+
+    for (int i = 0; i < 8; ++i) {
+        for (int k = 0; k < 8; ++k) {
+           // cout << i + a << k + b;
+            int A=i+a;
+            int B=k+b;
+
+            if (i % 2 == 0) {
+                // 첫칸이 흰색
+                if (k % 2 == 0) {
+                    // 흰색칸
+                    if(ff[A][B]){
+                        white++;
+                    }else{
+                        black++;
+                    }
+                } else {
+                    // 검정색칸
+                    if(ff[A][B]){
+                        black++;
+                    }else{
+                        white++;
+                    }
+                }
+            } else {
+                if (k % 2 == 0) {
+                    // 검정색칸
+                    if(ff[A][B]){
+                        black++;
+                    }else{
+                        white++;
+                    }
+                } else {
+                    // 흰색칸
+                    if(ff[A][B]){
+                        white++;
+                    }else{
+                        black++;
+                    }
+                }
+            }
+        }
+        //cout << endl;
+    }
+
+   // cout<<"흰"<<white<<endl;
+    //cout<<"검"<<black<<endl;
+
+    if(white>black){
+        return black;
+    }else{
+        return white;
+    }
+}
+
 
 int main() {
 
     queue<int> que;
-    int a;
-    cin >> a;
+    int a, b;
+    cin >> a >> b;
+
+
     for (int i = 0; i < a; ++i) {
-        string message;
-
-        cin >> message;
-
-        if (message == "push") {
-            int number;
-            cin >> number;
-            que.push(number);
-        } else if (message == "pop") {
-            if(que.empty()){
-                cout<<"-1"<<"\n";
-            }else{
-                cout << que.front() << "\n";
-                que.pop();
-            }
-
-        } else if (message == "size") {
-            cout << que.size() << "\n";
-        } else if (message == "empty") {
-            cout << que.empty() << "\n";
-        } else if (message == "front") {
-            if(que.empty()){
-                cout<<"-1"<<"\n";
-            }else{
-                cout << que.front() << "\n";
-            }
-        }else if (message == "back") {
-            if(que.empty()){
-                cout<<"-1"<<"\n";
-            }else{
-                cout << que.back() << "\n";
+        string line;
+        cin >> line;
+        for (int k = 0; k < b; ++k) {
+            if (line[k] == 'W') {
+                ff[i][k] = true;
+            } else if (line[k] == 'B') {
+                ff[i][k] = false;
             }
         }
-
     }
+
+
+   //int how = getHow(0, 0);
+    //cout << how;
+    int min=64;
+
+    for (int i = 0; i <= a-8; ++i) {
+        for (int k = 0; k <= b-8; ++k) {
+            //cout<<i<<k<<endl;
+            int how = getHow(i, k);
+            if(min>how){
+                min=how;
+            }
+
+        }
+    }
+    cout<<min;
+
+
+//    for (int i = 0; i < a; ++i) {
+//        for (int k = 0; k < b; ++k) {
+//            cout<< ff[i][k];
+//        }
+//        cout<<endl;
+//    }
+
+
 
 
     return 0;
