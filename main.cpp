e09203593e956e8a45b6d6b0c6921707da72eca6
oyuchan <oyc0401@gmail.com>
2022-07-16 04:29:26 +0900

    10989 수 정렬하기 3

--- main.cpp
+++ main.cpp
@@ -10,51 +10,27 @@
 
 using namespace std;
 
-map<int, int> setMap(int num) {
-    map<int, int> myMap;
-    while (num != 1) {
-        // cout << num << endl;
-        for (int i = 2;; ++i) {
-            if (num % i == 0) {
-                myMap[i]++;
-                num = num / i;
-                break;
-            }
-        }
-    }
-    return myMap;
-}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    int a, b;
+    int a;
+    int numbers[10001]{0};
 
-    cin >> a >> b;
-    map<int, int> mapA = setMap(a);
-    map<int, int> mapB = setMap(b);
-
-
-    // 최대 공약수 구하기
-    int maxGong = 1;
-
-    for (pair<int, int> po: mapB) {
-        int aNum = mapA[po.first];
-        int bNum = po.second;
-        if (aNum != 0) {
-            int min = aNum < bNum ? aNum : bNum;
-            for (int i = 0; i < min; ++i) {
-                maxGong *= po.first;
-            }
-        }
+    cin >> a;
+    int number;
+    for (int i = 0; i < a; ++i) {
+        cin >> number;
+        numbers[number]++;
     }
 
-    cout << maxGong<<endl;
-
-    /// 최소 공배수 구하기
-
-    int minGongbae = (a*b)/maxGong;
-
-    cout<<minGongbae;
+    for (int i = 1; i <= 10000; ++i) {
+        for (int k = 0; k < numbers[i]; ++k) {
+            cout << i << "\n";
+        }
+    }
 
 
     return 0;
