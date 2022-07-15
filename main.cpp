a6df8a840dc1e6f199c8bd6937e5c034c69a97db
oyuchan <oyc0401@gmail.com>
2022-07-16 03:41:28 +0900

    2609 최대 공약수, 최소 공배수

--- main.cpp
+++ main.cpp
@@ -5,30 +5,56 @@
 #include <set>
 #include <queue>
 #include <cmath>
+#include <map>
 
 
 using namespace std;
 
+map<int, int> setMap(int num) {
+    map<int, int> myMap;
+    while (num != 1) {
+        // cout << num << endl;
+        for (int i = 2;; ++i) {
+            if (num % i == 0) {
+                myMap[i]++;
+                num = num / i;
+                break;
+            }
+        }
+    }
+    return myMap;
+}
 
 int main() {
 
-    int a;
-    vector<int> vec;
+    int a, b;
 
-    cin>>a;
+    cin >> a >> b;
+    map<int, int> mapA = setMap(a);
+    map<int, int> mapB = setMap(b);
 
-    for (int i = 0; i < a; ++i) {
-        int number;
-        cin >> number;
-        vec.push_back(number);
+
+    // 최대 공약수 구하기
+    int maxGong = 1;
+
+    for (pair<int, int> po: mapB) {
+        int aNum = mapA[po.first];
+        int bNum = po.second;
+        if (aNum != 0) {
+            int min = aNum < bNum ? aNum : bNum;
+            for (int i = 0; i < min; ++i) {
+                maxGong *= po.first;
+            }
+        }
     }
 
+    cout << maxGong<<endl;
 
-   std::sort(vec.begin(), vec.end());
+    /// 최소 공배수 구하기
 
-    for (int num:vec) {
-        cout << num << "\n";
-    }
+    int minGongbae = (a*b)/maxGong;
+
+    cout<<minGongbae;
 
 
     return 0;
