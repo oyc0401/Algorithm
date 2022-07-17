55f53393071af7346d4f83e1dce5e1f271abcdff
oyuchan <oyc0401@gmail.com>
2022-07-17 20:36:49 +0900

    2798 블랙잭

--- main.cpp
+++ main.cpp
@@ -10,33 +10,42 @@
 
 using namespace std;
 
-
+// 1초: 1억번
 int main() {
 //    ios_base::sync_with_stdio(false);
 //    cin.tie(NULL);
 //    cout.tie(NULL);
 
-    int MOD = 1234567891;
-    int R = 31;
-    int a;
-    string text;
-    unsigned long long int sum = 0;
-    cin >> a >> text;
 
-    for (int i = 0; i < a; ++i) {
-        int data = text[i] - 'a' + 1;
-        // cout << data << endl;
-        unsigned long long pow = 1;
-        for (int j = 0; j < i; ++j) {
-            pow *= R;
-            pow %= MOD;
-        }
-        unsigned long long int num = data * pow;
-        //cout<<num<<endl;
-        sum += num;
-        sum %= MOD;
+    int index, number;
+    cin >> index >> number;
+
+    vector<int> vec;
+    int max = 0;
+
+
+    for (int i = 0; i < index; ++i) {
+        int a;
+        cin >> a;
+        vec.push_back(a);
     }
 
-    cout << sum;
+    for (int i = 0; i < vec.size(); ++i) {
+        int first = vec[i];
+        for (int j = i+1; j < vec.size(); ++j) {
+            int second = vec[j];
+            for (int k = j+1; k < vec.size(); ++k) {
+                int third = vec[k];
+                int sum = first + second + third;
+                if (sum <= number) {
+                    //cout << first << ", " << second << ", " << third<<endl;
+                    max = sum > max ? sum : max;
+                }
+            }
+        }
+    }
+
+    cout << max;
+
     return 0;
 }
