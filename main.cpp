4a3f90ff5813d177dffe2ce34d74a4bdb0fb8a56
oyuchan <oyc0401@gmail.com>
2022-07-18 05:29:47 +0900

    11650 좌표 정렬하기

--- main.cpp
+++ main.cpp
@@ -10,59 +10,31 @@
 
 using namespace std;
 
-int len, num;
-vector<bool> vec;
-
-int nextNumber(int where) {
-    for (int j = 0; j < num; ++j) {
-        int next = where + 1;
-        next %= len;
-        if (vec[next]) {
-//               cout<<"O";
-            where++;
-            where = where % len;
-        } else {
-            where++;
-            where %= len;
-
-            while (!vec[where]) {
-//                   cout<<"X";
-//                 cout<<where;
-                where++;
-                where = where % len;
-            }
-        }
-    }
-    return where;
-}
-
-/// 1초: 1억번
+// 1초: 1억번
 int main() {
-///    ios_base::sync_with_stdio(false);
-///    cin.tie(NULL);
-///    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    vector<int> numbers;
-    cin >> len >> num;
 
-    for (int i = 0; i < len; ++i) {
-        vec.push_back(true);
+
+
+    int index;
+    map<int, vector<int>> myMap;
+
+    cin >> index;
+
+    for (int i = 0; i < index; ++i) {
+        int x;
+        int y;
+        cin >> x >> y;
+        myMap[x].push_back(y);
     }
-    int where = -1;
-    for (int i = 0; i < len; ++i) {
-        where = nextNumber(where);
-        vec[where] = false;
-        numbers.push_back(where + 1);
-    }
 
-
-    cout << "<";
-    for (int i = 0; i < numbers.size(); ++i) {
-        cout << numbers[i];
-        if (i == numbers.size() - 1) {
-            cout << ">";
-        } else {
-            cout << ", ";
+    for (pair<int, vector<int>> pa: myMap) {
+        std::sort(pa.second.begin(), pa.second.end());
+        for (int y: pa.second) {
+            cout << pa.first <<" "<< y << "\n";
         }
     }
 
