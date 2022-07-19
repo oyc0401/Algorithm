8ec481959fad828947f8510d556c34c27a4ef398
oyuchan <oyc0401@gmail.com>
2022-07-20 00:06:40 +0900

    9095 1, 2, 3 더하기

--- main.cpp
+++ main.cpp
@@ -10,32 +10,33 @@
 
 using namespace std;
 
+int sum(int number) {
+
+
+    if (number == 1) {
+        return 1;
+    }
+    if (number == 2) {
+        return 2;
+    }
+    if (number == 3) {
+        return 4;
+    }
+
+    return sum(number - 1) + sum(number - 2) + sum(number - 3);
+}
+
 // 1초: 1억번
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
-
-
-
     int index;
-    map<int, vector<int>> myMap;
-
     cin >> index;
-
     for (int i = 0; i < index; ++i) {
-        int x;
-        int y;
-        cin >> x >> y;
-        myMap[y].push_back(x);
-    }
-
-    for (pair<int, vector<int>> pa: myMap) {
-        std::sort(pa.second.begin(), pa.second.end());
-        for (int x: pa.second) {
-            cout <<x<<" "<< pa.first << "\n";
-        }
+        int number;
+        cin >> number;
+        cout << sum(number) << "\n";
     }
 
 
