b4d09bcebdb0df1d8c5aa300221eb093cfd6e732
oyuchan <oyc0401@gmail.com>
2022-07-24 23:37:08 +0900

    13913 숨바꼭질 4

--- main.cpp
+++ main.cpp
@@ -10,94 +10,135 @@
 
 using namespace std;
 
-int my, you;
-int positions[300005]{};
-int inital = -12;
+int positions[300002];
 
-void printCurrent() {
-//    int big = my > you ? my : you;
+
+
+void printArr() {
 //    // 프린트
-//    for (int k = 0; k < big * 2; ++k) {
+//    for (int k = 0; k < you * 2; ++k) {
+//        // cout<<positions[i]<<", ";
+//        //cout << "[" << k << ": " << positions[k] << ']' << ", ";
 //        cout << k << ": " << "(" << positions[k] << ')' << ", ";
 //    }
 //    cout << "\n";
-
 }
 
-int getways(int my, int you) {
-
-
+int getNum(int my, int you) {
     if (my == you) {
+        //cout << 0 << endl;
         return 0;
     }
 
     vector<int> vec;
     vec.push_back(you);
     positions[you] = -1;
-    int yours = you;
-    while (yours != 0 && yours % 2 == 0) {
-        yours /= 2;
-        positions[yours] = 0;
-        vec.push_back(yours);
-    }
-
-
-    printCurrent();
 
 
     for (int i = 1; true; ++i) {
-       /// cout << "i: " << i << endl;
+        //cout << "i: " << i << endl;
 
         // 내 위치의 시간이 결정되면 끝
-        if (positions[my] != inital) {
+        if (positions[my] != 0) {
+            //cout << "답: " << positions[my] << endl;
             return positions[my];
+
         }
 
         // 새로운 위치를 추가
         vector<int> tempVec;
-        if (positions[my] == inital) {
+        if (positions[my] == 0) {
             for (int j = 0; j < vec.size(); ++j) {
                 int pos = vec[j];
                 //cout <<"vec: "<< pos << endl;
                 int a = pos - 1;
                 int b = pos + 1;
 
-
-                // -1칸
-                if (a >= 0 && positions[a] == inital) {
+                if (a >= 0 && positions[a] == 0) {
                     positions[a] = i;
                     tempVec.push_back(a);
-                    while (a != 0 && a % 2 == 0) {
-                        a /= 2;
-                        positions[a] = i;
-                        tempVec.push_back(a);
-                    }
                 }
-
-
-                // +1칸
-                if (positions[b] == inital) {
+                if (positions[b] == 0) {
                     positions[b] = i;
                     tempVec.push_back(b);
-                    while (b != 0 && b % 2 == 0) {
-                        b /= 2;
-                        positions[b] = i;
-                        tempVec.push_back(b);
+                }
+                if (pos % 2 == 0) {
+                    int c = pos / 2;
+                    if (c != 0 && positions[c] == 0) {
+                        positions[c] = i;
+                        tempVec.push_back(c);
                     }
                 }
-
             }
             // vec을 temp로 바꿈
             vec.clear();
-           /// cout << "추가된 원소: ";
+            //cout << "추가된 원소: ";
             for (int val: tempVec) {
-              ///  cout << val << ", ";
+                // cout << val << ", ";
                 vec.push_back(val);
             }
-          ///  cout << "\n";
+            //cout << "\n";
 
         }
-        printCurrent();
+
+
+    }
+
+}
+
+vector<int> posList(int my, int you) {
+    int currentPosition = my;
+    vector<int> vec;
+
+
+    for (int i = 1; true; ++i) {
+        vec.push_back(currentPosition);
+       // cout << currentPosition << " ";
+        //cout << "i: " << i << endl;
+
+        // 내 위치의 시간이 결정되면 끝
+        if (currentPosition == you) {
+            return vec;
+        }
+        int bigNumber = 100000000;
+        int a = currentPosition - 1;
+        int b = currentPosition + 1;
+        int c = currentPosition * 2;
+
+        int A = bigNumber;
+        int B = bigNumber;
+        int C = bigNumber;
+
+        // A 설정
+        if (a >= 0) {
+            if (positions[a] != 0) {
+                A = positions[a];
+            }
+        }
+
+        // B 설정
+        if (positions[b] != 0) {
+            B = positions[b];
+        }
+
+        // C 설정
+        if (currentPosition != 0 && positions[c] != 0) {
+            C = positions[c];
+        }
+
+
+        int small = A < B ? A : B;
+        small = small < C ? small : C;
+
+
+        if (small == A) {
+            currentPosition = a;
+        } else if (small == B) {
+            currentPosition = b;
+        } else if (small == C) {
+            currentPosition = c;
+        }
+
 
     }
 }
@@ -107,14 +148,18 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    int my, you;
     cin >> my >> you;
 
-    for (int &n: positions) {
-        n = inital;
+
+    int a = getNum(my, you);
+    cout << a << endl;
+    vector<int> myVec = posList(my, you);
+
+    for (int pos: myVec) {
+        cout << pos << " ";
     }
 
-    cout << getways(my, you);
 
     return 0;
 }
