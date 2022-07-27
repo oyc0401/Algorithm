dfdaf3fc308e0dd5b0c2722f3a516740acf7767e
oyuchan <oyc0401@gmail.com>
2022-07-28 06:10:16 +0900

    11279 최대 힙

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main() {
 
     int ind;
     cin >> ind;
-    priority_queue<int, vector<int>, greater<int>> que;
+    priority_queue<int, vector<int>, less<int>> que;
     for (int i = 0; i < ind; ++i) {
         int num;
         cin >> num;
