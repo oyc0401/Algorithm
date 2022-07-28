7b394b46ebd6672c8a40079830587125d919fcce
oyuchan <oyc0401@gmail.com>
2022-07-29 05:49:03 +0900

    11723 집합

--- main.cpp
+++ main.cpp
@@ -11,84 +11,58 @@
 using namespace std;
 
 
-//4
-//1 0 0 0
-//0 1 0 0
-//0 0 1 1
-//0 0 1 1
-
-
-
-int white = 0;
-int blue = 0;
-
-
-void cut(int x, int y, int size, int **arr) {
-    // 다른게 섞이면 자르기
-    int thisSection = arr[y][x];
-
-    ///cout<<"("<<x<<", "<<y<<"), 길이: "<<size<<", 이번 색: "<<thisSection<<endl;
-
-//    for (int i = y; i < y + size; ++i) {
-//        for (int j = x; j < x + size; ++j) {
-//            cout<<arr[i][j]<<" ";
-//        }
-//        cout << endl;
-//    }
-//    cout<<endl;
-
-    if(size>1){
-        for (int i = y; i < y + size; ++i) {
-            for (int j = x; j < x + size; ++j) {
-                // 다른게 섞이면 실패
-                if (arr[i][j] != thisSection) {
-                    int half = size / 2;
-                    cut(x, y, half, arr);
-                    cut(x + half, y, half, arr);
-                    cut(x, y + half, half, arr);
-                    cut(x + half, y + half, half, arr);
-                    return;
-                }
-
-            }
-        }
-    }
-
-
-    if (thisSection == 0) {
-        //cout<<"white +1\n";
-        white++;
-    } else {
-       // cout<<"blue +1\n";
-        blue++;
-    }
-    //cout<<endl;
-
-
-}
-
 // 1초: 1억번
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int width;
-    cin >> width;
+    map<int, bool> myMap;
 
-    int **arr = new int *[width]; //선언하고자 하는 이차원 배열의 행의 수 만큼 동적 할당
-    for (int i = 0; i < width; i++) //각각의 인덱스에 선언하고자 하는 배열의 크기만큼을 가르키게 함.
-        arr[i] = new int[width];
+    int index;
+    cin >> index;
 
-    for (int i = 0; i < width; ++i) {
-        for (int j = 0; j < width; ++j) {
-            cin >> arr[i][j];
+
+    for (int i = 0; i < index; ++i) {
+        string order;
+
+        cin >> order;
+        int num;
+
+
+        if (order == "add") {
+            cin >> num;
+
+            myMap[num] = true;
+        } else if (order == "remove") {
+            cin >> num;
+
+            myMap[num] = false;
+        } else if (order == "check") {
+            cin >> num;
+
+            cout << myMap[num] << '\n';
+
+        } else if (order == "toggle") {
+            cin >> num;
+
+            if (myMap[num]) {
+                myMap[num] = false;
+            } else {
+                myMap[num] = true;
+            }
+
+        } else if (order == "all") {
+            for (int j = 1; j <= 20; ++j) {
+                myMap[j] = true;
+            }
+
+        } else if (order == "empty") {
+            for (int j = 1; j <= 20; ++j) {
+                myMap[j] = false;
+            }
         }
     }
-
-    cut(0, 0, width, arr);
-
-    cout<<white<<endl<<blue;
 
 
     return 0;
