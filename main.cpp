19141d3cbbe440d39003efa96e191e036af7f629
oyuchan <oyc0401@gmail.com>
2022-07-30 07:40:55 +0900

    1167 트리의 지름

--- main.cpp
+++ main.cpp
@@ -10,48 +10,81 @@
 
 using namespace std;
 
-int min(int a, int b) {
-    return a < b ? a : b;
+
+vector<pair<int, int>> node[100001];
+bool check[100001];
+
+pair<int, int> parTree(int position, int lenght) {
+    vector<pair<int, int>> &vec = node[position];
+
+
+    pair<int, int> target = make_pair(position, lenght);
+    for (pair<int, int> pa: vec) {
+
+        if (!check[pa.first]) {
+            ///cout << "po: " << position << ", l: " << pa.first << " " << lenght +pa.second << endl;
+            check[pa.first] = true;
+            pair<int, int> pai = parTree(pa.first, lenght + pa.second);
+
+            // 더 큰거 저장
+            if (target.second <= pai.second) {
+                target = pai;
+            }
+        }
+    }
+
+    return target;
+
 }
+
 
 // 1초: 1억번
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int arr[1001][3];
-
     int index;
     cin >> index;
 
 
+
     /// 입력
-    for (int i = 0; i < index; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            cin >> arr[i][j];
+    for (int i = 1; i <= index; ++i) {
+        int po;
+        int a;
+        int b;
+        cin >> po >> a;
+        while (a != -1) {
+            cin >> b;
+            pair<int, int> pa = make_pair(a, b);
+            node[po].push_back(pa);
+
+            cin >> a;
         }
     }
 
-    int sum[1001][3];
-    for (int i = 0; i < 3; ++i) {
-        sum[0][i] = arr[0][i];
+    int start=1;
+    check[start] = true;
+    pair<int, int> pa = parTree(start, 0);
+
+    ///cout << pa.first << ", " << pa.second << endl;
+
+    // 두번째
+    start = pa.first;
+
+    for (bool &b: check) {
+        b = false;
     }
-    /// cout << sum[0][0] << ", " << sum[0][1] << ", " << sum[0][2] << endl;
 
-    for (int i = 1; i < index; ++i) {
-        sum[i][0] = min(sum[i - 1][1], sum[i - 1][2]) + arr[i][0];
-        sum[i][1] = min(sum[i - 1][0], sum[i - 1][2]) + arr[i][1];
-        sum[i][2] = min(sum[i - 1][0], sum[i - 1][1]) + arr[i][2];
+    check[start] = true;
+    pair<int, int> value = parTree(start, 0);
 
-        /// cout << sum[i][0] << ", " << sum[i][1] << ", " << sum[i][2] << endl;
-    }
+    ///cout << value.first << ", " << value.second << endl;
 
-    /// cout << sum[index][0] << ", " << sum[index][1] << ", " << sum[index][2];
 
-    int last = index - 1;
-    int min1 = min(sum[last][0], sum[last][1]);
-    min1 = min(min1, sum[last][2]);
-    cout << min1;
+    cout<<value.second;
+
+
 }
 
 
