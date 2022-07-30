3590a46cca8b41caaeb259ebb9b3ca54baaa23ec
oyuchan <oyc0401@gmail.com>
2022-07-31 05:33:51 +0900

    2448 별 찍기 - 11

--- main.cpp
+++ main.cpp
@@ -10,25 +10,61 @@
 
 using namespace std;
 
+//string star[11][3072]{"  *  ", " * * ", "*****"};
+//void setStar(int level) {
+//    int count = 3 * pow(2, level);
+//
+//    // 위 부분
+//    for (int i = 0; i < count / 2; ++i) {
+//        string tex;
+//        for (int j = 0; j < count / 2; ++j) {
+//            tex = tex + " ";
+//        }
+//        tex = tex + star[level - 1][i];
+//        for (int j = 0; j < count / 2; ++j) {
+//            tex = tex + " ";
+//        }
+//        star[level][i] = tex;
+//    }
+//
+//    // 아래부분
+//    for (int i = 0; i < count / 2; ++i) {
+//        string tex = star[level - 1][i] + " " + star[level - 1][i];
+//        star[level][i + (count / 2)] = tex;
+//    }
+//}
 
-int a, b;
-vector<int> vec;
 
-void print(int n, string text, int depth) {
+void printStar(int level, int row) {
 
-    for (int i = n; i < a; ++i) {
-        // cout << "def: " << def << ", n: " << n << endl;
-        string te = text + to_string(vec[i]) + " ";
-
-        if (depth == b) {
-            cout << te << endl;
+    if (level == 0) {
+        if (row == 1) {
+            cout << "  *  ";
+        } else if (row == 2) {
+            cout << " * * ";
         } else {
-            print(i , te, depth + 1);
+            cout << "*****";
+        }
+    } else {
+        int countHalf = 3 * pow(2, level) / 2;
+
+        // 윗 부분
+        if (row <= countHalf) {
+            for (int k = 1; k <= countHalf; ++k) {
+                cout << ' ';
+            }
+            printStar(level - 1, row);
+            for (int k = 1; k <= countHalf; ++k) {
+                cout << ' ';
+            }
+        } else {
+            // 아랫부분
+            printStar(level - 1, row - countHalf);
+            cout << ' ';
+            printStar(level - 1, row - countHalf);
         }
     }
 }
-
-
 
 // 1초: 1억번
 int main() {
@@ -36,17 +72,24 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int row;
+    cin >> row;
 
-    cin >> a >> b;
-
-    for (int i = 0; i < a; ++i) {
-        int num;
-        cin>>num;
-        vec.push_back(num);
+    int two = row / 3;
+    int level = 0;
+    while (two != 1) {
+        two /= 2;
+        level++;
     }
 
-    std::sort(vec.begin(), vec.end());
 
-    print(0, "", 1);
+    //int testL = 2;
+    //int testR = 3 * pow(2, testL);
+
+
+    for (int i = 1; i <= row; ++i) {
+        printStar(level, i);
+        cout << '\n';
+    }
 
 }
