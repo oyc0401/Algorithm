193441cb1bdc855f57492eb202bb727fee1c51a3
oyuchan <oyc0401@gmail.com>
2022-08-09 00:04:54 +0900

    11660 구간 합 구하기 5

--- main.cpp
+++ main.cpp
@@ -1,87 +1,79 @@
 #include <iostream>
+
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <cmath>
 #include <map>
-#include "vector"
+#include <cassert>
 
 using namespace std;
 
-// 100,000 이하의 소인수 분해
 
-void setPrimeNumbers(int n, vector<int> &primes) {
-    bool *isPrime = new bool[n + 1];// n까지 구해야하므로, n+1개를 동적할당
+// 1초: 1억번
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    //먼저 모든 배열을 true로 초기화
-    for (int i = 0; i <= n; i++) {
-        isPrime[i] = true;
+    int size, ind;
+    cin >> size >> ind;
+
+    int **arr = new int *[size]; //선언하고자 하는 이차원 배열의 행의 수 만큼 동적 할당
+
+    for (int i = 0; i < size; i++) //각각의 인덱스에 선언하고자 하는 배열의 크기만큼을 가르키게 함.
+        arr[i] = new int[size];
+
+    for (int k = 0; k < size; ++k) {
+        int a;
+        cin >> a;
+
+        if (k == 0) {
+            arr[0][k] = a;
+        } else {
+            arr[0][k] = arr[0][k - 1] + a;
+        }
     }
 
-    for (int i = 2; i <= n; i++) {
-        if (isPrime[i]) {
-            primes.push_back(i);//해당수가 소수라면, 출력하고 해당수를 제외한 배수들을 모두 제외
-            // cout << i << " ";
-            for (int j = i * 2; j <= n; j += i) {
-                isPrime[j] = false;
+    for (int i = 1; i < size; ++i) {
+
+        for (int k = 0; k < size; ++k) {
+            int a;
+            cin >> a;
+
+            if (k == 0) {
+                arr[i][k] = arr[i - 1][k] + a;
+            } else {
+                arr[i][k] = arr[i - 1][k] + arr[i][k - 1] + a - arr[i - 1][k - 1];
             }
         }
+
     }
-}
 
+    // 답 구하기
+    for (int i = 0; i < ind; ++i) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
 
-void factorization(int number, map<int, int> &myMap, vector<int> &primes) {
-    for (int prime: primes) {
+        x1--;
+        y1--;
+        x2--;
+        y2--;
 
-        int sum = 0;
-        for (long long int power = prime; power <= number; power *= prime) {
-            // cout<<sum<<endl;
-            sum += (number / power);
+        int a = arr[x2][y2];
+        if (y1 != 0) {
+            a -= arr[x2][y1 - 1];
         }
-        myMap[prime] = sum;
+        if (x1 != 0) {
+            a -= arr[x1 - 1][y2];
+        }
+        if (y1 != 0 && x1 != 0) {
+            a += arr[x1 - 1][y1 - 1];
+        }
+        cout << a << "\n";
     }
+
+
 }
-// 937482931 264783641 100000
 
-int main() {
-    map<int, int> myMap;
-    map<int, int> aMap;
-    map<int, int> bMap;
-
-    int num;
-    cin >> num;
-    vector<int> primes;
-
-    setPrimeNumbers(501, primes);
-
-    /// nCm = n! / ((n-m)! * m!)
-    factorization(num, myMap, primes);
-
-    int a = myMap[2];
-    int b = myMap[5];
-    int small = a < b ? a : b;
-    cout << small;
-//    factorization(n - m, aMap, primes);
-//    factorization(m, bMap, primes);
-//
-//
-//    // 분모 나누기
-//    for (pair<int, int> itr: aMap) {
-//        myMap[itr.first] -= itr.second;
-//    }
-//    for (pair<int, int> itr: bMap) {
-//        myMap[itr.first] -= itr.second;
-//    }
-//
-//    unsigned long long int num = 1;
-//    //출력
-//    for (pair<int, int> itr: myMap) {
-//        for (int i = 0; i < itr.second; i++) {
-//            num *= itr.first;
-//        }
-//
-//        // cout << itr.first << ": " << itr.second << endl;
-//    }
-
-    //cout << num;
-
-
-    return 0;
-}
 
