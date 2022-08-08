b22fa1f6523afccea08cb9064dd8762c1609580c
oyuchan <oyc0401@gmail.com>
2022-08-08 23:01:43 +0900

    2407 조합 (이 코드는 unsigned long long int 이상의 수를 못 다룸)

--- main.cpp
+++ main.cpp
@@ -1,126 +1,82 @@
 #include <iostream>
-
-#include <algorithm>
-#include <set>
-#include <queue>
-#include <cmath>
 #include <map>
-#include <stack>
-#include <cassert>
+#include "vector"
 
 using namespace std;
 
-class arrow {
-public:
-    arrow(int node, int target, int weight) : node(node), target(target), weight(weight) {
+// 100,000 이하의 소인수 분해
 
+void setPrimeNumbers(int n, vector<int> &primes) {
+    bool *isPrime = new bool[n + 1];// n까지 구해야하므로, n+1개를 동적할당
+
+    //먼저 모든 배열을 true로 초기화
+    for (int i = 0; i <= n; i++) {
+        isPrime[i] = true;
     }
 
-    string to_string() {
-        string a = "(" + std::to_string(node) + ", " + std::to_string(target)
-                   + ", " + std::to_string(weight) + ")";
-        return a;
-    }
-
-    int node, target, weight;
-private:
-
-};
-
-int dist[501];
-vector<arrow> vec;
-const int INF = 10000000;
-int nodeNum, roadNum, wormNum;
-
-bool bell() {
-    dist[1] = 0;
-    for (int i = 0; i < nodeNum; ++i) {
-        for (arrow arr: vec) {
-            if ( dist[arr.target] > dist[arr.node] + arr.weight) {
-                //cout<<arr.to_string()<<endl;
-                dist[arr.target] = dist[arr.node] + arr.weight;
-                if (i == nodeNum - 1) {
-                    return true;
-                }
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            primes.push_back(i);//해당수가 소수라면, 출력하고 해당수를 제외한 배수들을 모두 제외
+            // cout << i << " ";
+            for (int j = i * 2; j <= n; j += i) {
+                isPrime[j] = false;
             }
         }
     }
-    return false;
-}
-
-void going() {
-
-    vec.clear();
-
-    // 거리 초기화
-    for (int i = 0; i < 501; ++i) {
-        dist[i] = INF;
-    }
-
-    cin >> nodeNum >> roadNum >> wormNum;
-
-    for (int i = 0; i < roadNum; ++i) {
-        int node, target, weight;
-        cin >> node >> target >> weight;
-        vec.push_back(arrow(node, target, weight));
-        vec.push_back(arrow(target, node, weight));
-    }
-    for (int i = 0; i < wormNum; ++i) {
-        int node, target, weight;
-        cin >> node >> target >> weight;
-        vec.push_back(arrow(node, target, -weight));
-
-    }
-
-//    /// 간선 프린트
-//    for (int i = 0; i < vec.size(); ++i) {
-//        cout << vec[i].to_string() << ", ";
-//    }
-//    cout << endl;
-//    /// 간선 프린트
-
-
-    bool isCycle = bell();
-
-
-    if (isCycle) {
-        // 음수 순환 발생
-        cout << "YES";
-    } else {
-        // 음수 순환 안발생
-        cout << "NO";
-    }
-    cout << endl;
-
-//    /// 거리 프린트
-//    for (int i = 1; i <= nodeNum; ++i) {
-//        cout << dist[i] << ", ";
-//    }
-//    /// 거리 프린트
 }
 
 
-// 1초: 1억번
+void factorization(int number, map<int, int> &myMap, vector<int> &primes) {
+    for (int prime: primes) {
+
+        int sum = 0;
+        for (long long int power = prime; power <= number; power *= prime) {
+            // cout<<sum<<endl;
+            sum += (number / power);
+        }
+        myMap[prime] = sum;
+    }
+}
+// 937482931 264783641 100000
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    map<int, int> myMap;
+    map<int, int> aMap;
+    map<int, int> bMap;
 
-    int ind;
-    cin>>ind;
+    int n, m;
+    cin >> n >> m;
+    vector<int> primes;
 
-    for (int i = 0; i < ind; ++i) {
-        going();
+    setPrimeNumbers(100, primes);
+
+    /// nCm = n! / ((n-m)! * m!)
+    factorization(n, myMap, primes);
+    factorization(n - m, aMap, primes);
+    factorization(m, bMap, primes);
+
+
+    // 분모 나누기
+    for (pair<int, int> itr: aMap) {
+        myMap[itr.first] -= itr.second;
+    }
+    for (pair<int, int> itr: bMap) {
+        myMap[itr.first] -= itr.second;
     }
 
-}
-//3 3 1
-//1 2 2
-//1 3 4
-//2 3 1
-//3 1 3
+    unsigned long long int num = 1;
+    //출력
+    for (pair<int, int> itr: myMap) {
+        for (int i = 0; i < itr.second; i++) {
+            num *= itr.first;
+        }
 
-//3 2 1
-//1 2 3
-//2 3 4
-//3 1 8
+        // cout << itr.first << ": " << itr.second << endl;
+    }
+
+    cout << num;
+
+
+    return 0;
+}
+
