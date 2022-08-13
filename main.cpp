d00e1b0fbda0032ad2297dd37b4c3d2acbb0cd49
oyuchan <oyc0401@gmail.com>
2022-08-13 13:33:14 +0900

    13460 구슬 탈출 2 [테스트 게임]

--- main.cpp
+++ main.cpp
@@ -12,57 +12,204 @@
 
 using namespace std;
 
-int node, ind, start;
-vector<int> edge[1001];
+class quadruple {
+public:
+    int Rx, Ry;
+    int Bx, By;
 
-bool DFS_check[1001];
-bool BFS_check[1001];
+    string toString() {
+        return "(" + to_string(Rx) + ", " + to_string(Ry) + "), (" + to_string(Bx) + ", " + to_string(By) + ")";
+    }
+};
 
 
-//stack<int> printStack;
-queue<int> printDFS;
-queue<int> printQue;
+string *board;
 
-void DFS(int num) {
+enum direction {
+    top, bottom, left, right
+};
 
-    /// cout << num << " ";
-    DFS_check[num] = true;
+void MoveR(quadruple &current, int mX, int mY) {
+    // 빨강
+    if (board[current.Ry][current.Rx] == 'O') {
+        return;
+    }
+    if (board[current.Ry + mY][current.Rx + mX] == 'O') {
+        current.Ry += mY;
+        current.Rx += mX;
+        return;
+    }
 
-    for (int n: edge[num]) {
-        if (!DFS_check[n]) {
-            printDFS.push(n);
-            DFS(n);
+    while (board[current.Ry + mY][current.Rx + mX] == '.'
+           && !(current.Ry + mY == current.By && current.Rx + mX == current.Bx)) {
+        current.Ry += mY;
+        current.Rx += mX;
+        if (board[current.Ry + mY][current.Rx + mX] == 'O') {
+            current.Ry += mY;
+            current.Rx += mX;
+            return;
         }
     }
 
+}
+
+void MoveB(quadruple &current, int mX, int mY) {
+    // 파랑
+    if (board[current.By][current.Bx] == 'O') {
+        return;
+    }
+    if (board[current.By + mY][current.Bx + mX] == 'O') {
+        current.By += mY;
+        current.Bx += mX;
+        return;
+    }
+
+    while (board[current.By + mY][current.Bx + mX] == '.'
+           && !(current.By + mY == current.Ry && current.Bx + mX == current.Rx)) {
+        current.By += mY;
+        current.Bx += mX;
+
+        if (board[current.By + mY][current.Bx + mX] == 'O') {
+            current.By += mY;
+            current.Bx += mX;
+            return;
+        }
+    }
 
 }
 
-void BFS(int num) {
-    queue<int> que;
+quadruple tilt(quadruple current, direction direc) {
 
-    // 처음 큐 넣기
-    que.push(num);
+    int mY;
+    int mX;
 
-    // 큐 순환
-    while (!que.empty()) {
-        /// cout<<que.front()<<" ";
+    switch (direc) {
 
-        if (!BFS_check[que.front()]) {
-            BFS_check[que.front()] = true;
-            printQue.push(que.front());
+        case direction::top:
+            mY = -1;
+            mX = 0;
+            if (current.Ry < current.By) {
+                MoveR(current, mX, mY);
+                MoveB(current, mX, mY);
+            } else {
+                MoveB(current, mX, mY);
+                MoveR(current, mX, mY);
+            }
+            return current;
 
-            for (int n: edge[que.front()]) {
-                que.push(n);
+        case direction::bottom:
+            mY = +1;
+            mX = 0;
+            if (current.Ry > current.By) {
+                MoveR(current, mX, mY);
+                MoveB(current, mX, mY);
+            } else {
+                MoveB(current, mX, mY);
+                MoveR(current, mX, mY);
+            }
+            return current;
+        case direction::left:
+            mY = 0;
+            mX = -1;
+            if (current.Rx < current.Bx) {
+                MoveR(current, mX, mY);
+                MoveB(current, mX, mY);
+            } else {
+                MoveB(current, mX, mY);
+                MoveR(current, mX, mY);
+            }
+            return current;
+        case direction::right:
+            mY = 0;
+            mX = +1;
+            if (current.Rx > current.Bx) {
+                MoveR(current, mX, mY);
+                MoveB(current, mX, mY);
+            } else {
+                MoveB(current, mX, mY);
+                MoveR(current, mX, mY);
+            }
+            return current;
+    }
+
+}
+
+
+//void BFS(int num) {
+//    queue<int> que;
+//
+//    // 처음 큐 넣기
+//    que.push(num);
+//
+//    // 큐 순환
+//    while (!que.empty()) {
+//        /// cout<<que.front()<<" ";
+//
+//        if (!BFS_check[que.front()]) {
+//            BFS_check[que.front()] = true;
+//            printQue.push(que.front());
+//
+//            for (int n: edge[que.front()]) {
+//                que.push(n);
+//            }
+//        }
+//
+//        que.pop();
+//    }
+//
+//
+//}
+
+
+void startGame(quadruple moved,int N,int M){
+    /// print
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            if (moved.Rx == j && moved.Ry == i && moved.Bx == j && moved.By == i) {
+                cout << "P" << " ";
+            } else if (moved.Rx == j && moved.Ry == i) {
+                cout << "R" << " ";
+            } else if (moved.Bx == j && moved.By == i) {
+                cout << "B" << " ";
+            } else {
+                cout << board[i][j] << " ";
             }
         }
-
-        que.pop();
+        cout << endl;
     }
 
+    char where;
+    cin >> where;
+    while (where != 'q') {
 
+        if (where == 'w') {
+            moved = tilt(moved, direction::top);
+        } else if (where == 's') {
+            moved = tilt(moved, direction::bottom);
+        } else if (where == 'a') {
+            moved = tilt(moved, direction::left);
+        } else if (where == 'd') {
+            moved = tilt(moved, direction::right);
+        }
+
+        /// print
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < M; ++j) {
+                if (moved.Rx == j && moved.Ry == i && moved.Bx == j && moved.By == i) {
+                    cout << "P" << " ";
+                } else if (moved.Rx == j && moved.Ry == i) {
+                    cout << "R" << " ";
+                } else if (moved.Bx == j && moved.By == i) {
+                    cout << "B" << " ";
+                } else {
+                    cout << board[i][j] << " ";
+                }
+            }
+            cout << endl;
+        }
+        cin >> where;
+    }
 }
-
 
 // 1초: 1억번
 int main() {
@@ -70,35 +217,56 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> node >> ind >> start;
+    int N, M;
+    cin >> N >> M;
 
-    for (int i = 0; i < ind; ++i) {
-        int a, b;
-        cin >> a >> b;
-        edge[a].push_back(b);
-        edge[b].push_back(a);
+    board = new string[N]; //선언하고자 하는 이차원 배열의 행의 수 만큼 동적 할당
+
+    for (int i = 0; i < N; ++i) {
+        cin >> board[i];
     }
 
-    // 정렬
-    for (vector<int> &v: edge) {
-        std::sort(v.begin(), v.end());
+    quadruple position{};
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            if (board[i][j] == 'R') {
+                position.Ry = i;
+                position.Rx = j;
+                board[i][j] = '.';
+            }
+            if (board[i][j] == 'B') {
+                position.By = i;
+                position.Bx = j;
+                board[i][j] = '.';
+            }
+        }
     }
 
 
-    printDFS.push(start);
-    DFS(start);
-    while (!printDFS.empty()) {
-        cout << printDFS.front() << " ";
-        printDFS.pop();
-    }
+//    for (int i = 0; i < N; ++i) {
+//        cout << board[i] << " ";
+//        cout << endl;
+//    }
+//
+//    cout << "처음: " << position.toString() << endl;
 
-    cout<<"\n";
 
-    BFS(start);
-    while (!printQue.empty()) {
-        cout << printQue.front() << " ";
-        printQue.pop();
-    }
+    startGame(position,N,M);
+
+
+//    quadruple moved = tilt(position, direction::top);
+//    cout << moved.toString() << endl;
+//
+//
+//    moved = tilt(moved, direction::left);
+//    cout << moved.toString() << endl;
+//
+//    moved = tilt(moved, direction::right);
+//    cout << moved.toString() << endl;
+//
+//    moved = tilt(moved, direction::bottom);
+//    cout << moved.toString() << endl;
 
     return 0;
 }
