911229a1401098bcdc4954831a5bb2e6c4ab4fe0
oyuchan <oyc0401@gmail.com>
2022-08-15 02:33:20 +0900

    1918 후위 표기식

--- main.cpp
+++ main.cpp
@@ -10,40 +10,145 @@
 
 using namespace std;
 
-void printStar(int level, int row) {
-
-    if (level == 1) {
-        if (row == 1) {
-            cout << "***";
-        } else if (row == 2) {
-            cout << "* *";
+// B+C
+// A+B+C
+//A+BCx
+string backPlus(string text) {
+    string val = "";
+    stack<char> sign;
+    for (int i = 0; i < text.size(); ++i) {
+        if (text[i] == '+' || text[i] == '-') {
+            sign.push(text[i]);
+        } else if (text[i] == '(' || text[i] == ')') {
+            val += '.';
         } else {
-            cout << "***";
+            val += text[i];
         }
-    } else {
-        // 123 456 789
-        int countHalf = pow(3, level) / 3;
-
-        // 중간 부분
-        if (countHalf < row && row <= countHalf * 2) {
-
-            printStar(level - 1, row - countHalf);
-
-            for (int k = 1; k <= countHalf; ++k) {
-                cout << ' ';
-            }
-
-            printStar(level - 1, row - countHalf);
-
-        } else {
-
-            for (int i = 0; i < 3; ++i) {
-                printStar(level - 1, row % countHalf);
-            }
-        }
-
     }
+
+    while (!sign.empty()) {
+        if (sign.top() == '+') {
+            val += 'p';
+        } else if (sign.top() == '-') {
+            val += 'm';
+        }
+        sign.pop();
+    }
+
+    return val;
 }
+
+
+string backTime(string text) {
+    string val = "";
+    stack<char> sign;
+    for (int i = 0; i < text.size(); ++i) {
+        if (text[i] == '*' || text[i] == '/') {
+            sign.push(text[i]);
+        } else if (text[i] == '(' || text[i] == ')') {
+            val += '.';
+        } else {
+            val += text[i];
+        }
+    }
+
+    while (!sign.empty()) {
+        if (sign.top() == '*') {
+            val += 'x';
+        } else if (sign.top() == '/') {
+            val += 'd';
+        }
+        sign.pop();
+    }
+
+    return val;
+}
+
+
+
+
+//string brackets(string text) {
+//
+//    int lastS = 0;
+//    bool findMulti = false;
+//    stack<int> sta;
+//    for (int i = 0; i < text.size(); ++i) {
+//       // cout<<text<<endl;
+//        if (text[i] == '*' || text[i] == '/') {
+////            cout<<i<<" ";
+//            findMulti = true;
+//        }
+//
+//        if (text[i] == '(') {
+//            sta.push(i);
+//        } else if (text[i] == ')') {
+//            sta.pop();
+//        }
+//
+//
+//        if (text[i] == '+' || text[i] == '-') {
+//            if (sta.empty()&&findMulti) {
+//                text.insert(lastS , "(");
+//                text.insert(i + 1, ")");
+//                i += 2;
+//                findMulti = false;
+//            }
+//            if (sta.empty()) {
+//                lastS = i+1;
+//            }
+//        }
+//
+//    }
+//
+//    if (findMulti) {
+//        text.insert(lastS , "(");
+//        text.insert(text.size(), ")");
+//    }
+//
+//    text.insert(0 , "(");
+//    text.insert(text.size(), ")");
+//    return text;
+//}
+// +: p, -: m, *: x, /: d
+
+// A+B*C*D/E-
+
+//string brackets(string text) {
+//
+//    int lastS = 0;
+//    bool findMulti = false;
+//    stack<int> sta;
+//    sta.push(0);
+//    for (int i = 0; i < text.size(); ++i) {
+//        if(text[i] == '+' || text[i] == '-'){
+//            if(findMulti){
+//                text.insert(sta.top(),"(");
+//                text.insert(i+1,")");
+//                i+=2;
+//                lastS=i;
+//            }else{
+//                lastS=i;
+//                findMulti= true;
+//            }
+//
+//        }else if(text[i] == '*' || text[i] == '/'){
+//
+//            stack<int> stst;
+//            if(text[i+1]=='('){
+//                st
+//            }
+//            text.insert(lastS+1,"(");
+//            text.insert(i+3,")");
+//            i+=2;
+//        }
+//
+//
+//    }
+//
+//
+//    return text;
+//}
+
 
 // 1초: 1억번
 int main() {
@@ -51,28 +156,119 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int row;
-    cin >> row;
+    string text;
+    cin >> text;
 
-    int val = row;
-    int level = 0;
 
-    // val이 3^2 이면 level=2;
-    while (val != 1) {
-        val /= 3;
-        level++;
+    stack<char> sta;
+
+
+    for (int i = 0; i < text.size(); ++i) {
+        //cout<<i<<" ";
+
+        if (text[i] == '+' || text[i] == '-') {
+
+            if (sta.empty() || sta.top() == '(') {
+                sta.push(text[i]);
+            } else {
+                while (!sta.empty() && sta.top() != '(') {
+                    cout << sta.top();
+                    sta.pop();
+                }
+                sta.push(text[i]);
+            }
+
+        } else if (text[i] == '*' || text[i] == '/') {
+
+            if (sta.empty() || sta.top() == '(') {
+                sta.push(text[i]);
+            } else {
+                while (!sta.empty() && (sta.top() == '*' || sta.top() == '/')) {
+                    cout << sta.top();
+                    sta.pop();
+                }
+                sta.push(text[i]);
+            }
+
+        } else if (text[i] == '(') {
+
+            sta.push('(');
+
+
+        } else if (text[i] == ')') {
+
+            while (!sta.empty() && sta.top() != '(') {
+                cout << sta.top();
+                sta.pop();
+            }
+            if (sta.top() == '(') {
+                sta.pop();
+            }
+
+        } else {
+            cout << text[i];
+        }
+
+
+    }
+    while (!sta.empty()) {
+        if (sta.top() != '(') {
+            cout << sta.top();
+        }
+        sta.pop();
     }
 
 
-    //int testL = 2;
-    //int testR = 3 * pow(2, testL);
 
 
-    //cout << level;
+//    text = brackets(text);
+//    cout << text << endl;
 
-    for (int i = 1; i <= row; ++i) {
-        printStar(level, i);
-        cout << '\n';
-    }
+    // (A*(B+C))
+    // A*B+C
+
+//    stack<int> sta;
+//    int size = text.size();
+//    for (int i = 0; i < size; ++i) {
+//
+//        if (text[i] == '(') {
+//            sta.push(i);
+//        } else if (text[i] == ')') {
+//            int last = sta.top();
+//            // 3~7
+//            string start = text.substr(0, last);
+//            string finish = text.substr(i + 1, size - i);
+//            //cout<<start<<", "<<finish<<endl;
+//
+//            string st = text.substr(last, i - last + 1);
+//            st = backPlus(st);
+//            st = backTime(st);
+//
+//            text = start + st + finish;
+//
+//            cout << st << ", " << text << endl;
+//            sta.pop();
+//        }
+//
+//    }
+//
+//
+//    for (int i = 0; i < text.size(); ++i) {
+//        if (text[i] == '.') {
+//
+//        } else if (text[i] == 'p') {
+//            cout << '+';
+//        } else if (text[i] == 'm') {
+//            cout << '-';
+//        } else if (text[i] == 'x') {
+//            cout << '*';
+//        } else if (text[i] == 'd') {
+//            cout << '/';
+//        } else {
+//            cout << text[i];
+//        }
+//
+//    }
+
 
 }
