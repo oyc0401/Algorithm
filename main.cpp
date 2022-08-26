312ed89f1d0b8e2523bf423630fbc6232ea4f50e
oyuchan <oyc0401@gmail.com>
2022-08-27 00:04:14 +0900

    2869 달팽이는 올라가고 싶다

--- main.cpp
+++ main.cpp
@@ -9,36 +9,7 @@
 #include <cassert>
 
 using namespace std;
-int x, y, inventory;
-int **arr;
 
-int MAX=1000000000;
-
-int dig(int height) {
-
-
-    int inven = inventory;
-    int time = 0;
-
-
-    for (int i = 0; i < y; ++i) {
-        for (int k = 0; k < x; ++k) {
-            if (arr[i][k] < height) {
-                // 땅이 낮으면 설치
-                inven -= height - arr[i][k];
-                time += height - arr[i][k];
-            } else if (arr[i][k] > height) {
-                // 땅이 높으면 부수기
-                inven += arr[i][k] - height;
-                time += (arr[i][k] - height) * 2;
-            }
-        }
-    }
-    if (inven < 0) {
-        time = MAX;
-    }
-    return time;
-}
 
 // 1초: 1억번
 int main() {
@@ -46,60 +17,23 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int A, B, V;
 
-    cin >> y >> x >> inventory;
+    cin >> A >> B >> V;
 
+    V -= A;
 
-    arr = new int *[y]; //행의 크기가 3인 이차원 배열
-    for (int i = 0; i < y; i++) //각각의 행에 길이가 4인 배열을 할당
-        arr[i] = new int[x];
-
-
-    for (int i = 0; i < y; ++i) {
-        for (int j = 0; j < x; ++j) {
-            cin >> arr[i][j];
-        }
+    if (V < 0) {
+        cout << 1;
     }
 
 
-    int minTime = MAX;
-    int high = 0;
-
-    for (int i = 0; i < 300; ++i) {
-        int t = dig(i);
-        if (minTime >= t) {
-            minTime = t;
-            high = i;
-        }
-        //cout << t << " " << i << endl;
-
-//        if(t==MAX){
-//            break;
-//        }
-
+    int day = (V / (A - B)) + 1;
+    if ((V % (A - B)) != 0) {
+        day++;
     }
 
-    cout<<minTime<<" "<<high;
-
-
-
-
-//    /// print
-//    for (int i = 0; i < y; ++i) {
-//        for (int j = 0; j < x; ++j) {
-//            cout << arr[i][j] << " ";
-//        }
-//        cout << endl;
-//    }
+    cout << day << endl;
 
 
 }
-//3 4 0
-//64 64 64 64
-//64 64 64 64
-//64 64 64 63
-
-//3 4 99
-//0 0 0 1
-//0 0 0 1
-//0 0 1 1
