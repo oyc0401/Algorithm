f04ba6c7b8dc6a2a0d50279d76150e89c04f3dbc
oyuchan <oyc0401@gmail.com>
2022-09-03 21:33:39 +0900

    split 함수 개발

--- main.cpp
+++ main.cpp
@@ -7,70 +7,22 @@
 #include <map>
 #include <stack>
 #include <cassert>
+#include<sstream>
 
 using namespace std;
 
-bool is_prime(long long int number) {
-    if(number==0||number==1){
-        return false;
+
+vector<string> split(string text, char div) {
+    istringstream ss(text);
+    string stringBuffer;
+    vector<string> vec;
+    vec.clear();
+
+    // 자르기
+    while (getline(ss, stringBuffer, div)) {
+        vec.push_back(stringBuffer);
     }
-
-    int sq = sqrt(number);
-    for (int i = 2; i <= sq; ++i) {
-        if (number % i == 0) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-int solution(int n, int k){
-    stack<int> sta;
-
-    while (n != 0) {
-        int nod = n % k;
-        sta.push(nod);
-        n /= k;
-    }
-
-
-    vector<int> vec;
-    while (!sta.empty()) {
-     ///  cout<<sta.top();
-        vec.push_back(sta.top());
-        sta.pop();
-    }
-   ///cout<<endl;
-
-    long long int num=0;
-    vector<long long int> list;
-
-    for (int ele: vec) {
-///cout<<num<<endl;
-        if (ele == 0) {
-            list.push_back(num);
-            num = 0;
-        } else {
-            num = num * 10 + ele;
-        }
-    }
-   /// cout<<num<<endl;
-   if(num!=0){
-       list.push_back(num);
-   }
-
-    int sum = 0;
-    for (long long int ele: list) {
-      ///  cout << ele << endl;
-        if (is_prime(ele)) {
-            sum++;
-        }
-
-    }
-
-    return sum;
-
+    return vec;
 }
 
 
@@ -81,15 +33,14 @@ int main() {
     cout.tie(NULL);
 
 
+    vector<string> x = split("kakao naver google", ' ');
 
+    cout << endl << "vector 값을 출력해보자." << endl;
 
-    int n, k;
-    cin >> n >> k;
-
-    cout<<solution(n,k);
+    for (int i = 0; i < x.size(); i++) {
+        cout << x[i] << endl;
+    }
 
 }
-// 437674 3
 
-// 859156 3
 
