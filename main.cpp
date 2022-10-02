088229db9ae0f0adbeee0dc62594c62fe362d9d3
oyuchan <oyc0401@gmail.com>
2022-10-02 22:10:19 +0900

    25711 인경산

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-
+#include <iomanip>
 #include <algorithm>
 #include <set>
 #include <queue>
@@ -10,167 +10,85 @@
 
 using namespace std;
 
-class Point {
-public:
-    int x;
-    int y;
-    int h;
-};
-
 // 1초: 1억번
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    cout << fixed << setprecision(8);
 
-    int x, y, h;
-    cin >> x >> y >> h;
+    int len, index;
 
+    vector<int> vecX;
+    vector<int> vecY;
 
-    int ***arr = new int **[h];
-    for (int i = 0; i < h; i++) {
-        arr[i] = new int *[y];
-        for (int j = 0; j < y; ++j) {
-            arr[i][j] = new int [x];
-        }
+    cin >> len >> index;
+
+    for (int i = 0; i < len; ++i) {
+        int a;
+        cin >> a;
+        vecX.push_back(a);
+    }
+    for (int i = 0; i < len; ++i) {
+        int b;
+        cin >> b;
+        vecY.push_back(b);
     }
 
+    vector<double> heL{0};
+    vector<double> heR{0};
 
+    double lastL = 0;
+    double lastR = 0;
 
-    queue<Point> que;
-    for (int k = 0; k < h; ++k) {
-        for (int i = 0; i < y; ++i) {
-            for (int j = 0; j < x; ++j) {
-                int a;
-                cin >> a;
-                arr[k][i][j] = a;
-                if (a == 1) {
-                    Point point;
-                    point.h = k;
-                    point.y = i;
-                    point.x = j;
-                    que.push(point);
-                }
-            }
+    for (int i = 0; i < len - 1; ++i) {
+        long long int X = vecX[i + 1] - vecX[i];
+        long long int Y = vecY[i + 1] - vecY[i];
+
+        long long int XY = X * X + Y * Y;
+        double dist = sqrt(XY);
+
+        if (Y > 0) {
+            heL.push_back(lastL + dist * 3);
+            heR.push_back(lastR + dist);
+            lastL += dist * 3;
+            lastR += dist;
+        } else if (Y == 0) {
+            heL.push_back(lastL + dist * 2);
+            heR.push_back(lastR + dist * 2);
+            lastL += dist * 2;
+            lastR += dist * 2;
+        } else {
+            heL.push_back(lastL + dist);
+            heR.push_back(lastR + dist * 3);
+            lastL += dist;
+            lastR += dist * 3;
         }
-    }
-
-
-    int sum = 0;
-
-    while (!que.empty()) {
-        int size = que.size();
-        bool isChanged = false;
-
-        for (int i = 0; i < size; ++i) {
-            Point pa = que.front();
-            que.pop();
-            const int X = pa.x;
-            const int Y = pa.y;
-            const int H = pa.h;
-
-            // cout<<"i: "<<i<<", x: "<<X<<", y: "<<Y<<", sum: "<<sum<<endl;
-            //cout << "sum: " << sum << ", x: " << X << ", y: " << Y << endl;
-            // 왼쪽
-            if (X > 0 && arr[H][Y][X - 1] == 0) {
-                arr[H][Y][X - 1] = 1;
-                Point tuple;
-                tuple.h = H;
-                tuple.x = X - 1;
-                tuple.y = Y;
-
-                que.push(tuple);
-                isChanged = true;
-            }
-            // 오른쪽
-            if (X < x - 1 && arr[H][Y][X + 1] == 0) {
-                arr[H][Y][X + 1] = 1;
-                Point tuple;
-                tuple.h = H;
-                tuple.x = X + 1;
-                tuple.y = Y;
-                que.push(tuple);
-                isChanged = true;
-            }
-            // 위쪽
-            if (Y > 0 && arr[H][Y - 1][X] == 0) {
-                arr[H][Y - 1][X] = 1;
-                Point tuple;
-                tuple.h = H;
-                tuple.x = X;
-                tuple.y = Y - 1;
-                que.push(tuple);
-                isChanged = true;
-            }
-            // 아래쪽
-            if (Y < y - 1 && arr[H][Y + 1][X] == 0) {
-                arr[H][Y + 1][X] = 1;
-                Point tuple;
-                tuple.h = H;
-                tuple.x = X;
-                tuple.y = Y + 1;
-                que.push(tuple);
-                isChanged = true;
-            }
-            // 천장
-            if (H > 0 && arr[H - 1][Y][X] == 0) {
-                arr[H - 1][Y][X] = 1;
-                Point tuple;
-                tuple.h = H - 1;
-                tuple.x = X;
-                tuple.y = Y;
-                que.push(tuple);
-                isChanged = true;
-            }
-            // 바닥
-            if (H < h - 1 && arr[H + 1][Y][X] == 0) {
-                arr[H + 1][Y][X] = 1;
-                Point tuple;
-                tuple.h = H + 1;
-                tuple.x = X;
-                tuple.y = Y;
-                que.push(tuple);
-                isChanged = true;
-            }
-        }
-
-        if (isChanged) {
-            sum++;
-        }
-
-
-
-//        /// print
-//        for (int k = 0; k < h; ++k) {
-//            for (int i = 0; i < y; ++i) {
-//                for (int j = 0; j < x; ++j) {
-//                    cout << arr[k][i][j] << " ";
-//                }
-//                cout << endl;
-//            }
-//        }
-//
-//        /// print
 
 
     }
 
-    bool complete = true;
-    for (int k = 0; k < h; ++k) {
-        for (int i = 0; i < y; ++i) {
-            for (int j = 0; j < x; ++j) {
-                if (arr[k][i][j] == 0) {
-                    complete = false;
-                }
-            }
+//    for (double d: heL) {
+//        cout << d << " ";
+//    }
+//    cout << endl;
+//    for (double d: heR) {
+//        cout << d << " ";
+//    }
+//    cout << endl;
+
+
+
+    for (int i = 0; i < index; ++i) {
+        int start, finish;
+        cin >> start >> finish;
+        if (start < finish) {
+            cout << heL[finish - 1] - heL[start - 1] << '\n';
+        } else {
+            cout << heR[start - 1] - heR[finish - 1] << '\n';
         }
-    }
 
 
-    if (complete) {
-        cout << sum;
-    } else {
-        cout << -1;
     }
 
     return 0;
