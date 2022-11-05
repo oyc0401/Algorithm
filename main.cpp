824b0c6a0a1423067b738621ec1caf51190be7ab
oyuchan <oyc0401@gmail.com>
2022-11-05 23:39:13 +0900

    2468 안전 영역

--- main.cpp
+++ main.cpp
@@ -16,63 +16,65 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int dx[8] = {1, -1, 0, 0,1,1,-1,-1};
-    int dy[8] = {0, 0, 1, -1,1,-1,1,-1};
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
 
     int x, y;
-    cin >> x >> y;
+    cin >> x;
+    y = x;
 
-    while (x != 0 || y != 0) {
 
+    int **arr = new int *[y];
+    for (int i = 0; i < y; ++i) {
+        arr[i] = new int[x];
+    }
 
-        int **arr = new int *[y];
-        for (int i = 0; i < y; ++i) {
-            arr[i] = new int[x];
+    for (int i = 0; i < y; ++i) {
+        for (int j = 0; j < x; ++j) {
+            cin >> arr[i][j];
         }
+    }
 
-        for (int i = 0; i < y; ++i) {
-            for (int j = 0; j < x; ++j) {
-                cin >> arr[i][j];
+
+    vector<int> vec;
+    for (int blank = 0; blank <= 100; ++blank) {
+
+        bool visted[101][101];
+        for (int i = 0; i < 101; ++i) {
+            for (int j = 0; j < 101; ++j) {
+                visted[i][j] = false;
             }
         }
 
-//    /// print
-//    for (int i = 0; i < y; ++i) {
-//        for (int j = 0; j < x; ++j) {
-//            cout << arr[i][j] << " ";
-//        }
-//        cout << '\n';
-//    }
-//    cout << '\n';
-//    /// print
-
-
-        // bool visted[9][9];
         queue<pair<int, int>> que;
 
 
         int sum = 0;
         for (int i = 0; i < y; ++i) {
             for (int j = 0; j < x; ++j) {
-                if (arr[i][j] == 1) {
-                    sum++;
-                    //cout<<": "<<j<<", "<<i<<endl;
+                if (arr[i][j] > blank&&!visted[i][j]) {
+
+                    //  cout<<": "<<j<<", "<<i<<endl;
 
                     que.push(make_pair(j, i));
-                    arr[i][j] = 0;
+                    visted[i][j] = true;
+                    sum++;
+
+                    // 인접한거 제거
                     while (!que.empty()) {
                         int a = que.front().first;
                         int b = que.front().second;
-                        for (int k = 0; k < 8; ++k) {
+                        for (int k = 0; k < 4; ++k) {
                             int nx = a + dx[k];
                             int ny = b + dy[k];
-                           // cout<<"??: "<<a<<", "<<b<<endl;
+                            // cout<<"??: "<<a<<", "<<b<<endl;
                             //cout<<"n: "<<nx<<", "<<ny<<endl;
                             if (0 <= nx && 0 <= ny && nx < x && ny < y
-                                && arr[ny][nx] == 1) {
+                                && arr[ny][nx] > blank && !visted[ny][nx]) {
 
+                                //cout<<"?";
                                 que.push(make_pair(nx, ny));
-                                arr[ny][nx] = 0;
+                                visted[ny][nx] = true;
                             }
                         }
                         que.pop();
@@ -83,10 +85,32 @@ int main() {
             }
         }
 
-        cout << sum<<'\n';
-        cin >> x >> y;
+        //cout << sum << '\n';
+        vec.push_back(sum);
+
+
     }
 
+    int max = vec[0];
+    for (int n: vec) {
+        if (max < n) {
+            max = n;
+        }
+
+    }
+
+    cout << max;
+
+
+//        /// print
+//    for (int i = 0; i < y; ++i) {
+//        for (int j = 0; j < x; ++j) {
+//            cout << arr[i][j] << " ";
+//        }
+//        cout << '\n';
+//    }
+//    cout << '\n';
+//    /// print
 
     return 0;
 }
