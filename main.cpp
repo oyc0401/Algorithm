9d2cebc77d7d6c8f4152d707e9dd0e0087b78272
oyuchan <oyc0401@gmail.com>
2023-03-29 16:29:17 +0900

    11404 플로이드

--- main.cpp
+++ main.cpp
@@ -10,29 +10,14 @@
 
 using namespace std;
 
-long long mod = 1000000007;
+int INF = 100000000;
 
-class Matrix {
+class Node {
 public:
-    Matrix(long long a, long long b, long long c, long long d)
-            : a(a), b(b), c(c), d(d) {}
+    Node(int n, int w) : node(n), weight(w) {}
 
-    long long a, b, c, d;
-
-    Matrix operator*(Matrix &m) {
-        long long newA = a * m.a + b * m.c;
-        long long newB = a * m.b + b * m.d;
-        long long newC = c * m.a + d * m.c;
-        long long newD = c * m.b + d * m.d;
-
-        newA %= mod;
-        newB %= mod;
-        newC %= mod;
-        newD %= mod;
-
-        return Matrix{newA, newB, newC, newD};
-    }
-
+    int node;
+    int weight;
 };
 
 // 1초: 1억번
@@ -41,21 +26,132 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    long long n;
-    cin >> n;
+    int count, lenght;
+    cin >> count >> lenght;
 
-    Matrix answer{1, 0, 0, 1};
-    Matrix a{1, 1, 1, 0};
-
-    while (n > 0) {
-        if (n % 2 == 1)
-            answer = answer * a;
-        a = a * a;
-        n /= 2;
+    // arr
+    int **arr = new int *[count];
+    for (int i = 0; i < count; ++i) {
+        arr[i] = new int[count];
     }
 
-    cout << answer.b;
+    for (int i = 0; i < count; ++i) {
+        for (int j = 0; j < count; ++j) {
+            arr[i][j] = INF;
+        }
+        arr[i][i] = 0;
+    }
 
+    // graph
+    int **graph = new int *[count];
+    for (int i = 0; i < count; ++i) {
+        graph[i] = new int[count];
+    }
+
+    for (int i = 0; i < count; ++i) {
+        for (int j = 0; j < count; ++j) {
+            graph[i][j] = 0;
+        }
+    }
+
+
+    for (int i = 0; i < lenght; ++i) {
+        int a, b, weight;
+        cin >> a >> b >> weight;
+        a--;
+        b--;
+        if (graph[a][b] == 0) {
+            graph[a][b] = weight;
+        } else if (graph[a][b] > weight) {
+            graph[a][b] = weight;
+        }
+        if (arr[a][b] > weight) {
+            arr[a][b] = weight;
+        }
+
+
+    }
+
+
+//    cout << "<노드>\n";
+//    for (int i = 0; i < count; ++i) {
+//        for (int j = 0; j < count; ++j) {
+//            cout << graph[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    cout << "<가중치>\n";
+//    for (int i = 0; i < count; ++i) {
+//        for (int j = 0; j < count; ++j) {
+//            if (arr[i][j] == INF) {
+//                cout << "INF" << " ";
+//            } else {
+//                cout << arr[i][j] << " ";
+//            }
+//        }
+//        cout << endl;
+//    }
+//
+//    cout << endl;
+
+
+    for (int focusNode = 0; focusNode < count; ++focusNode) {
+        for (int k = 0; k < count; ++k) {
+            for (int next = 0; next < count; ++next) {
+
+                // focusNode = 1, k = 3, next = 2
+                // 3 -> 1 로의 가중치 + 1 -> 2 로의 가중치가
+                int sum = arr[k][focusNode] + arr[focusNode][next];
+                // 3 -> 2 로의 가중치 보다 작으면
+                if (sum < arr[k][next]) {
+
+//                    cout << "가중치: " << k << "->" << focusNode << "->" << next << " = " << sum << " <-> "
+//                         << arr[k][next] << endl;
+                    arr[k][next] = sum;
+//                    for (int i = 0; i < count; ++i) {
+//                        for (int j = 0; j < count; ++j) {
+//                            if (arr[i][j] == INF) {
+//                                cout << "INF" << " ";
+//                            } else {
+//                                cout << arr[i][j] << " ";
+//                            }
+//                        }
+//                        cout << endl;
+//                    }
+//                    cout << endl;
+                } else {
+//                    cout << "작지않다: " << k << "->" << focusNode << "->" << next << " = " << sum << " <-> "
+//                         << arr[k][next] << endl;
+                }
+
+
+            }
+
+
+        }
+
+    }
+
+//    cout << "<노드>\n";
+//    for (int i = 0; i < count; ++i) {
+//        for (int j = 0; j < count; ++j) {
+//            cout << graph[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    cout << "<가중치>\n";
+    for (int i = 0; i < count; ++i) {
+        for (int j = 0; j < count; ++j) {
+            if (arr[i][j] == INF) {
+                cout << "0" << " ";
+            } else {
+                cout << arr[i][j] << " ";
+            }
+        }
+        cout << endl;
+    }
 
     return 0;
 }
