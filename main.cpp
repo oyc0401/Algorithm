a2b5c250d942eb75e1b1aa743310132bc008f818
oyuchan <oyc0401@gmail.com>
2023-04-02 23:58:38 +0900

    14003 가장 긴 증가하는 부분 수열 5

--- main.cpp
+++ main.cpp
@@ -6,13 +6,80 @@
 #include <cmath>
 #include <map>
 #include <cassert>
+#include <stack>
 
 using namespace std;
 
-vector<int> myVec;
+
+template<typename T>
+class LinkedListStack {
+private:
+    struct Node {
+        T data;
+        Node *next;
+
+        Node(T d) : data(d), next(nullptr) {}
+    };
+
+    Node *topNode;
+    int _size;
+
+public:
+    LinkedListStack() : topNode(nullptr) {
+        _size = 0;
+    }
+
+    void push(T data) {
+        Node *newNode = new Node(data);
+        newNode->next = topNode;
+        topNode = newNode;
+        _size++;
+    }
+
+    void pop() {
+        if (topNode == nullptr) {
+            std::cerr << "Stack underflow\n";
+            return;
+        }
+
+        Node *temp = topNode;
+        topNode = topNode->next;
+        delete temp;
+        _size--;
+    }
+
+    int size() { return _size; }
+
+    T top() const {
+        if (topNode == nullptr) {
+            std::cerr << "Stack underflow\n";
+            return T();
+        }
+
+        return topNode->data;
+    }
+
+    bool empty() const {
+        return topNode == nullptr;
+    }
+
+    ~LinkedListStack() {
+        while (topNode != nullptr) {
+            Node *temp = topNode;
+            topNode = topNode->next;
+            delete temp;
+        }
+    }
+};
+
+
+LinkedListStack<pair<int, int>> myVec[1000001];
+
+
+int myVecSize = 0;
 
 int lastIdx(int target) {
-    int size = myVec.size();
+    int size = myVecSize;
     int left = 0;
     int right = size - 1;
 
@@ -22,10 +89,11 @@ int lastIdx(int target) {
 
 //        cout << left << ", " << mid << ", " << right << endl;
 
-        if (myVec[mid] > target) {
+        int midObject = myVec[mid].top().second;
+        if (midObject > target) {
             // 다운
             right = mid - 1;
-        } else if (myVec[mid] < target) {
+        } else if (midObject < target) {
             // 업
             left = mid + 1;
         } else {
@@ -34,73 +102,99 @@ int lastIdx(int target) {
         }
     }
 
-//    cout<<"리턴: "<<left<<endl;
     return left;
 }
 // 0 9
 // 1 3 6 8 10 13 17 18 20
 
-int minNum = -1'000'000'001;
+int minNum = -2'000'000'000;
+
+
 
 // 1000000000000000000
 // 1초: 1억번
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    cout.tie(NULL);
 
 
     int idx;
     cin >> idx;
-    queue<int> que;
+//    queue<int> que;
+    vector<int> vec;
 
-    myVec.push_back(minNum);
+    myVec[0].push(make_pair(-1, minNum));
+    myVecSize++;
 
     for (int i = 0; i < idx; ++i) {
         int a;
         cin >> a;
-        que.push(a);
+        vec.push_back(a);
     }
 
-    while (!que.empty()) {
 
-        /// cout<<qu.front()<<":\n";
+    int saveIdx = 1;
+//    vector<int> saveVec;
+    for (int i = 0; i < vec.size(); ++i) {
 
-        int cur = que.front();
+        // cout<<que.front()<<":\n";
+
+        int cur = vec[i];
         int last = lastIdx(cur);
-//        cout<<last<<endl;
 
-        if (myVec.size() <= last) {
-            myVec.push_back(que.front());
+        if (myVecSize <= last) {
+            myVec[myVecSize].push(make_pair(i, cur));
+            myVecSize++;
+
+
+
+            saveIdx = i;
         } else {
-            myVec[last] = que.front();
+            myVec[last].push(make_pair(i, cur));
+
         }
 
 
 
-//        /// print
-//        for (int i = 0; i < myVec.size(); ++i) {
-//            cout << "(" << i << ", " << myVec[i] << ") ";
-//        }
-//        cout << endl;
 
-        que.pop();
+//
+        /// print
+        for (int k = 0; k < myVecSize; ++k) {
+            cout << "(" << k << ", " << myVec[k].top().second << ", " << myVec[k].size() << ") ";
+        }
+        cout << endl;
 
     }
 
 
-    cout << myVec.size() - 1 << endl;
+//    cout << sizeof myVec[3] << endl;
+    cout << myVecSize - 1 << endl;
 
-//    for (int n: myVec) {
-//        if (n != minNum) {
-//            cout << n << " ";
-//        }
-//
-//    }
+
+    stack<int> printStack;
+
+    for (int i = myVecSize - 1; i > 0; --i) {
+
+        while (myVec[i].top().first > saveIdx) {
+            myVec[i].pop();
+        }
+        saveIdx = myVec[i].top().first;
+        if (myVec[i].top().second != minNum) {
+            printStack.push(myVec[i].top().second);
+        }
+    }
+
+    while (!printStack.empty()) {
+        cout << printStack.top() << " ";
+        printStack.pop();
+    }
+
 
 }
 
-//12
-//1 2 10 10 11 99 13 14 3 4 5 6
+//14
+//1 2 10 10 11 4 99 13 14 5 6 7 3 3
 
 /// 3 6 5 7 8 3 2 7 9 1 5 7 4 2 9 8 2 3 4 8 7 9
+
