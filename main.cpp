a033a2787514e29ec04ea04451198102f3925fcb
oyuchan <oyc0401@gmail.com>
2023-04-04 03:40:13 +0900

    11505 구간 곱 구하기 (세그먼트 트리)

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@
 
 using namespace std;
 
-long long mininit(vector<long long> &arr, vector<long long> &tree, int node, int start, int end) {
+int MOD = 1'000'000'007;
+
+long long init(vector<long long> &arr, vector<long long> &tree, int node, int start, int end) {
     if (start == end)    // 노드가 리프 노드인 경우
         return tree[node] = arr[start];    // 배열의 그 원소를 가져야 함
 
@@ -18,75 +20,47 @@ long long mininit(vector<long long> &arr, vector<long long> &tree, int node, int
 //    cout << start << " " << mid << " " << end << endl;
 
     // 구간 합을 구하는 경우
-//    return tree[node] = mininit(arr, tree, node * 2, start, mid) + mininit(arr, tree, node * 2 + 1, mid + 1, end);
+    return tree[node] = (init(arr, tree, node * 2, start, mid)
+                         * init(arr, tree, node * 2 + 1, mid + 1, end)) % MOD;
 
     // 구간의 최솟값을 구하는 경우도 비슷하게 해줄 수 있다.
-    return tree[node] = min(mininit(arr, tree, node * 2, start, mid), mininit(arr, tree, node * 2 + 1, mid + 1, end));
+    // return tree[node] = min(init(arr, tree, node * 2, start, mid), init(arr, tree, node * 2 + 1, mid + 1, end));
 }
 
-long long maxinit(vector<long long> &arr, vector<long long> &tree, int node, int start, int end) {
-    if (start == end)    // 노드가 리프 노드인 경우
-        return tree[node] = arr[start];    // 배열의 그 원소를 가져야 함
-
-    int mid = (start + end) / 2;
-//    cout << start << " " << mid << " " << end << endl;
-
-    // 구간 합을 구하는 경우
-//    return tree[node] = mininit(arr, tree, node * 2, start, mid) + mininit(arr, tree, node * 2 + 1, mid + 1, end);
-
-    // 구간의 최솟값을 구하는 경우도 비슷하게 해줄 수 있다.
-    return tree[node] = max(maxinit(arr, tree, node * 2, start, mid), maxinit(arr, tree, node * 2 + 1, mid + 1, end));
-}
-
-long long minsum(vector<long long> &tree, int node, int start, int end, int left, int right) {
+long long sum(vector<long long> &tree, int node, int start, int end, int left, int right) {
     // case 1: [start, end] 앞 뒤에 [left, right]가 있는 경우,
     // 겹치지 않기 때문에 탐색을 더 이상 할 필요가 없다.
-    if (left > end || right < start) return 1000000001;
+    if (left > end || right < start) return 1;
 
     // case 2: [start, end]가 [left, right]에 포함
     if (left <= start && end <= right) return tree[node];
 
     // case 3, 4: 왼쪽 자식과 오른쪽 자식을 루트로 하는 트리에서 다시 탐색 시작
     int mid = (start + end) / 2;
-    return min(minsum(tree, node * 2, start, mid, left, right), minsum(tree, node * 2 + 1, mid + 1, end, left, right));
-}
-
-long long maxsum(vector<long long> &tree, int node, int start, int end, int left, int right) {
-    // case 1: [start, end] 앞 뒤에 [left, right]가 있는 경우,
-    // 겹치지 않기 때문에 탐색을 더 이상 할 필요가 없다.
-    if (left > end || right < start) return 0;
-
-    // case 2: [start, end]가 [left, right]에 포함
-    if (left <= start && end <= right) return tree[node];
-
-    // case 3, 4: 왼쪽 자식과 오른쪽 자식을 루트로 하는 트리에서 다시 탐색 시작
-    int mid = (start + end) / 2;
-    return max(maxsum(tree, node * 2, start, mid, left, right), maxsum(tree, node * 2 + 1, mid + 1, end, left, right));
+    return (sum(tree, node * 2, start, mid, left, right)
+            * sum(tree, node * 2 + 1, mid + 1, end, left, right)) % MOD;
 }
 
 
-void update(vector<long long> &tree, int node, int start, int end, int index, long long diff) {
-    if (index < start || index > end) return;    // case 2
-    tree[node] = tree[node] + diff;    // case 1
+long long update(vector<long long> &tree, int node, int start, int end, int index, long long val) {
+    if (index > end || index < start) return tree[node];
 
-    // 리프 노드가 아닌 경우 자식도 변경해줘야 하기 때문에,
-    // 리프 노드인지 검사를 하고 아래 자식 노드를 갱신해준다.
-    if (start != end) {
-        int mid = (start + end) / 2;
-        update(tree, node * 2, start, mid, index, diff);
-        update(tree, node * 2 + 1, mid + 1, end, index, diff);
-    }
+    if (start == end) return tree[node] = val;
+
+    int mid = start + (end - start) / 2;
+    return tree[node] = (update(tree, 2 * node, start, mid, index, val) *
+                         update(tree, 2 * node + 1, mid + 1, end, index, val)) % MOD;
 }
 
 // 1000000000000000000
 // 1초: 1억번
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    cout.tie(NULL);
 
-    int len, sumLen;
-    cin >> len >> sumLen;
+    int len, updateLen, sumLen;
+    cin >> len >> updateLen >> sumLen;
 
     vector<long long> vec;
     for (int i = 0; i < len; ++i) {
@@ -96,11 +70,9 @@ int main() {
     }
 
 
-    vector<long long> minSegTree(4 * len);
-    vector<long long> maxSegTree(4 * len);
+    vector<long long> segTree(4 * len);
 
-    mininit(vec, minSegTree, 1, 0, len - 1);
-    maxinit(vec, maxSegTree, 1, 0, len - 1);
+    init(vec, segTree, 1, 0, len - 1);
 
 //    for (long long n: segTree) {
 //        cout << n << " ";
@@ -108,16 +80,24 @@ int main() {
 //    cout << endl;
 
 
-    for (int i = 0; i < sumLen; ++i) {
-        int a, b;
-        cin >> a >> b;
+    for (int i = 0; i < updateLen + sumLen; ++i) {
+        long long a, b, c;
+        cin >> a >> b >> c;
+        if (a == 1) {
+            // 업데이트
+            long long val = c;
+            int index = b - 1;
+            long long diff = val;
+            vec[index] = val;
+            update(segTree, 1, 0, len - 1, index, diff);
 
-        int left = a - 1;
-        int right = b - 1;
-        long long minS = minsum(minSegTree, 1, 0, len - 1, left, right);
-        long long maxS = maxsum(maxSegTree, 1, 0, len - 1, left, right);
+        } else if (a == 2) {
+            int left = b - 1;
+            int right = c - 1;
+            long long plus = sum(segTree, 1, 0, len - 1, left, right);
 
-        cout << minS << " " << maxS << '\n';
+            cout << plus << endl;
+        }
     }
 
 
