b0d3bafd61a0913b6d75ef9e16e4a2de9aa92c19
oyuchan <oyc0401@gmail.com>
2023-04-06 19:23:12 +0900

    1197 최소 스패닝 트리 (최소 스패닝 트리)

--- main.cpp
+++ main.cpp
@@ -10,66 +10,25 @@
 
 using namespace std;
 
-int len, sumLen;
+class Node {
+public:
 
-long long mininit(vector<long long> &arr, vector<long long> &tree, int node, int start, int end) {
-    if (start == end)    // 노드가 리프 노드인 경우
-        return tree[node] = start;    // 배열의 그 원소를 가져야 함
+    int a, b, w;
 
-    int mid = (start + end) / 2;
-//    cout << start << " " << mid << " " << end << endl;
-
-    // 구간 합을 구하는 경우
-//    return tree[node] = mininit(arr, tree, node * 2, start, mid) + mininit(arr, tree, node * 2 + 1, mid + 1, end);
-
-    // 구간의 최솟값을 구하는 경우도 비슷하게 해줄 수 있다.
-    long long a = mininit(arr, tree, node * 2, start, mid);
-    long long b = mininit(arr, tree, node * 2 + 1, mid + 1, end);
-    if (arr[a] <= arr[b]) {
-        return tree[node] = a;
-    } else {
-        return tree[node] = b;
+    void print() {
+        cout << a << " " << b << " " << w << endl;
     }
 
-}
-
-
-long long minsum(vector<long long> &arr, vector<long long> &tree, int node, int start, int end, int left, int right) {
-    // case 1: [start, end] 앞 뒤에 [left, right]가 있는 경우,
-    // 겹치지 않기 때문에 탐색을 더 이상 할 필요가 없다.
-    if (left > end || right < start) return 0;
-
-    // case 2: [start, end]가 [left, right]에 포함
-    if (left <= start && end <= right) return tree[node];
-
-    // case 3, 4: 왼쪽 자식과 오른쪽 자식을 루트로 하는 트리에서 다시 탐색 시작
-    int mid = (start + end) / 2;
-    long long a = minsum(arr, tree, node * 2, start, mid, left, right);
-    long long b = minsum(arr, tree, node * 2 + 1, mid + 1, end, left, right);
-    if (arr[a] <= arr[b]) {
-        return a;
-    } else {
-        return b;
+    bool operator<(const Node &loc2) const {
+        return w > loc2.w;
     }
+};
+
+
+bool compare(Node x, Node y) {
+    return x.w < y.w;
 }
 
-
-long long
-update(vector<long long> &arr, vector<long long> &tree, int node, int start, int end, int index, long long val) {
-    if (index > end || index < start) return tree[node];
-    if (start == end) return tree[node] = val;
-    int mid = start + (end - start) / 2;
-
-    long long a = update(arr, tree, node * 2, start, mid, index, val);
-    long long b = update(arr, tree, node * 2 + 1, mid + 1, end, index, val);
-    if (arr[a] <= arr[b]) {
-        return tree[node] = a;
-    } else {
-        return tree[node] = b;
-    }
-
-
-}
 
 
 // 1000000000000000000
@@ -79,53 +38,122 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int T, edgeCount;
+    vector<vector<pair<int, int>>> nodeArr;
+    long long sum = 0;
 
-    cin >> len;
+    cin >> T >> edgeCount;
 
-    vector<long long> vec;
-
-    vec.push_back(1000000001);
-    for (int i = 0; i < len; ++i) {
-        long long n;
-        cin >> n;
-        vec.push_back(n);
+    int visited[T];
+    for (int i = 0; i < T; ++i) {
+        visited[i] = false;
     }
 
-
-    vector<long long> minSegTree(4 * len);
-
-    mininit(vec, minSegTree, 1, 1, len);
+    nodeArr = vector<vector<pair<int, int>>>(T);
 
 
-//    for (long long n: minSegTree) {
-//        cout << n << " ";
+//    for (int i = 0; i < T; ++i) {
+//        for (int j = 0; j < T; ++j) {
+//            arr[i][j] = INF;
+//        }
+//        cout << endl;
+//    }
+
+
+
+//    cout << "노드 출력:" << endl;
+//    for (int i = 0; i < count; ++i) {
+//        for (int j = 0; j < count; ++j) {
+//            cout << node[i][j] << " ";
+//        }
+//        cout << endl;
 //    }
 //    cout << endl;
 
-//    long long minS = minsum(vec, minSegTree, 1, 0, len-1, 0, 4);
-//    cout << minS << '\n';
+    vector<Node> vec;
 
-    cin >> sumLen;
-
-    for (int i = 0; i < sumLen; ++i) {
-        int M, a, b;
-        cin >> M >> a >> b;
-        if (M == 1) {
-// 업데이트
-            int val = a;
-            int index = a - 1;
-            vec[a] = b;
-            update(vec, minSegTree, 1, 0, len - 1, index, val);
-        } else {
-            int left = a - 1;
-            int right = b - 1;
-            long long minS = minsum(vec, minSegTree, 1, 0, len - 1, left, right);
-//        long long maxS = maxsum(maxSegTree, 1, 0, len - 1, left, right);
-
-            cout << minS << '\n';
-        }
-
+    for (int i = 0; i < edgeCount; ++i) {
+        int a, b, w;
+        cin >> a >> b >> w;
+        a--;
+        b--;
+//        node[a][b] = w;
+//        node[b][a] = w;
+        Node node = Node();
+        node.a = a;
+        node.b = b;
+        node.w = w;
+        vec.push_back(node);
+        nodeArr[node.a].push_back(make_pair(node.b, node.w));
+        nodeArr[node.b].push_back(make_pair(node.a, node.w));
     }
 
 
+    std::sort(vec.begin(), vec.end(), compare);
+
+//    for (int i = 0; i < nodeCount; ++i) {
+//        vec[i].print();
+//    }
+
+
+    int minNode = vec[0].a;
+    visited[minNode] = true;
+
+
+    priority_queue<Node> que;
+    for (auto pa: nodeArr[minNode]) {
+        Node no = Node();
+        no.a = minNode;
+        no.b = pa.first;
+        no.w = pa.second;
+        que.push(no);
+//        cout << "push: ";
+//        no.print();
+    }
+    while (!que.empty()) {
+        if (visited[que.top().b] == false) {
+//            Node n = que.top();
+//            n.print();
+            sum += que.top().w;
+            visited[que.top().b] = true;
+
+            int topB = que.top().b;
+            que.pop();
+
+            for (auto pa: nodeArr[topB]) {
+                if (visited[pa.first] == false) {
+                    Node no = Node();
+                    no.a = topB;
+                    no.b = pa.first;
+                    no.w = pa.second;
+                    que.push(no);
+//                    cout << "push: ";
+//                    no.print();
+//                    cout << "top: " << que.top().a << " " << que.top().b << " " << que.top().w<<endl;
+                }
+
+            }
+        } else {
+//            cout << "거절: ";
+//            Node n = que.top();
+//            n.print();
+            que.pop();
+        }
+
+    }
+    cout << sum;
+
+    //5 5
+    //1 4 3
+    //2 3 3
+    //4 3 5
+    //2 1 6
+    //4 5 9
+
 }
+
+//3 2
+//1 2 1
+//2 3 2
+
+
