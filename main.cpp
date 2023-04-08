4300d7de658b6ce4d6faa5d23e9fc696beb55f0e
oyuchan <oyc0401@gmail.com>
2023-04-09 00:49:07 +0900

    save 1306 달려라 홍준

--- main.cpp
+++ main.cpp
@@ -10,23 +10,29 @@
 
 using namespace std;
 
-class Obj {
+class Square {
+public:
+    Square(int e = 0, int idx = 0) {
+        this->e = e;
+        this->index = idx;
+    }
+
     int e;
     int index;
 };
 
-template<typename T>
-class MinSegTree {
+//template<typename Square>
+class SegTree {
 public:
 
-    MinSegTree(vector<T> &arr) {
-        int len = arr.size();
-        tree = vector<T>(4 * len);
+    SegTree(vector<Square> &arr) {
+        len = arr.size();
+        tree = vector<Square>(4 * len);
         init(arr, 1, 0, len - 1);
     }
 
 
-    T init(vector<T> &arr, int node, int start, int end) {
+    Square init(vector<Square> &arr, int node, int start, int end) {
         if (start == end)    // 노드가 리프 노드인 경우
             return tree[node] = arr[start];    // 배열의 그 원소를 가져야 함
 
@@ -37,34 +43,64 @@ public:
 //    return tree[node] = mininit(arr, tree, node * 2, start, mid) + mininit(arr, tree, node * 2 + 1, mid + 1, end);
 
         // 구간의 최솟값을 구하는 경우도 비슷하게 해줄 수 있다.
-        return tree[node] = min(init(arr, node * 2, start, mid), init(arr, node * 2 + 1, mid + 1, end));
+
+        Square a = init(arr, node * 2, start, mid);
+        Square b = init(arr, node * 2 + 1, mid + 1, end);
+        if (a.e > b.e) {
+            return tree[node] = a;
+        } else {
+            return tree[node] = b;
+        }
     }
 
-    T sum(int node, int start, int end, int left, int right) {
+    Square sum(int node, int start, int end, int left, int right) {
         // case 1: [start, end] 앞 뒤에 [left, right]가 있는 경우,
         // 겹치지 않기 때문에 탐색을 더 이상 할 필요가 없다.
-        if (left > end || right < start) return 1000000001;
+        if (left > end || right < start) return Square(0, -1);
 
         // case 2: [start, end]가 [left, right]에 포함
         if (left <= start && end <= right) return tree[node];
 
         // case 3, 4: 왼쪽 자식과 오른쪽 자식을 루트로 하는 트리에서 다시 탐색 시작
         int mid = (start + end) / 2;
-        return min(sum(node * 2, start, mid, left, right), sum(node * 2 + 1, mid + 1, end, left, right));
+
+        Square a = sum(node * 2, start, mid, left, right);
+        Square b = sum(node * 2 + 1, mid + 1, end, left, right);
+        if (a.e > b.e) {
+            return a;
+        } else {
+            return b;
+        }
+
     }
 
-    T update(int node, int start, int end, int index, T val) {
-        if (index > end || index < start) return tree[node];
-        if (start == end) return tree[node] = val;
-        int mid = start + (end - start) / 2;
-        return tree[node] = min(update(node * 2, start, mid, index, val),
-                                update(node * 2 + 1, mid + 1, end, index, val));
+//    Square update(int node, int start, int end, int index, Square val) {
+//        if (index > end || index < start) return tree[node];
+//        if (start == end) return tree[node] = val;
+//        int mid = start + (end - start) / 2;
+//
+//        Square a = update(node * 2, start, mid, index, val);
+//        Square b = update(node * 2 + 1, mid + 1, end, index, val);
+//        if (a.e < b.e) {
+//            return tree[node] = a;
+//        } else {
+//            return tree[node] = b;
+//        }
+//
+//    }
 
+
+    void print() {
+        for (Square e: tree) {
+            cout << e.e << " ";
+        }
+        cout << endl;
     }
 
 
 private:
-    vector<T> tree;
+    int len;
+    vector<Square> tree;
 };
 
 
@@ -75,37 +111,40 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int len, sumLen;
-    cin >> len;
 
-    vector<long long> vec;
+    int len, width;
+    int maxSize = -1;
+    cin >> len >> width;
+
+    vector<Square> vec;
     for (int i = 0; i < len; ++i) {
         int n;
         cin >> n;
-        vec.push_back(n);
-    }
-    cin >> sumLen;
-
-    MinSegTree<long long> segTree(vec);
-
-    for (int i = 0; i < sumLen; ++i) {
-        int M, a, b;
-        cin >> M >> a >> b;
-        if (M == 1) {
-            // 업데이트
-            long long val = b;
-            int index = a - 1;
-            vec[index] = val;
-            segTree.update(1, 0, len - 1, index, val);
-        } else {
-            int left = a - 1;
-            int right = b - 1;
-            long long minS = segTree.sum(1, 0, len - 1, left, right);
-
-            cout << minS << '\n';
-        }
-
+        Square square = Square();
+        square.e = n;
+        square.index = i;
+        vec.push_back(square);
     }
 
 
+    SegTree segTree(vec);
+
+//    segTree.print();
+
+    for (int i = 0; i < len - width; ++i) {
+
+        int start = i;
+        int end = i + 2 * (width - 1);
+//        cout << start << " " << end << endl;
+        Square s = segTree.sum(1, 0, len - 1, start, end);
+        cout << s.e << " ";
+    }
+
+//    Square s = segTree.sum(1, 0, len - 1, start, end);
+
+
+//    cout << maxSize << endl;
 }
+
+//8
+//1 2 3 4 5 6 7 8
